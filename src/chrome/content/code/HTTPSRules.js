730f0ca2dd2b8b0a6258c1ac79720db868c83ff8
Peter Eckersley <pde@eff.org>
2011-01-19 11:46:09 -0800

    Change the "Enable All" button to "Reset Defaults"
    This is probably what the user wants, since some of the rules that are off by
    default are quite buggy.  The "Enable All" button is actually still lurking
    there, but the XUL doesn't seem to support an extra3 button.  Does XUL have
    some arbitrary limit on the number of buttons it supports?

--- src/chrome/content/code/HTTPSRules.js
+++ src/chrome/content/code/HTTPSRules.js
@@ -17,7 +17,7 @@ function CookieRule(host, cookiename) {
 }
 
 function RuleSet(name, match_rule, default_off) {
-  var on_by_default = true;
+  this.on_by_default = true;
   this.name = name;
   this.ruleset_match = match_rule;
   if (match_rule) {
@@ -29,7 +29,7 @@ function RuleSet(name, match_rule, default_off) {
     // Perhaps problematically, this currently ignores the actual content of
     // the default_off XML attribute.  Ideally we'd like this attribute to be
     // "valueless"
-    on_by_default = false;
+    this.on_by_default = false;
   }
   this.rules = [];
   this.exclusions = [];
@@ -42,7 +42,7 @@ function RuleSet(name, match_rule, default_off) {
     // if not, create it
     this.log(DBUG, "Creating new pref " + name);
     this.active = true;
-    prefs.setBoolPref(name, on_by_default);
+    prefs.setBoolPref(name, this.on_by_default);
   }
 }
 
