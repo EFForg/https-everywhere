b84ee880aeb39078e5db48b7ff976d5efac46802
Mike Perry <mikeperry-git fscked org>
2011-03-10 19:23:09 -0800

    Add a base64 encode step for display..

--- src/components/ssl-observatory.js
+++ src/components/ssl-observatory.js
@@ -131,19 +131,25 @@ SSLObservatory.prototype = {
    }
 
    if ("http-on-examine-response" == topic) {
-     aSubject.QueryInterface(Ci.nsIHttpChannel);
-     var certchain = this.getSSLCert(aSubject);
+     subject.QueryInterface(Ci.nsIHttpChannel);
+     var certchain = this.getSSLCert(subject);
      if(certchain) {
-       for(cert in certchain.getChain()) {
-         var fp = cert.md5Fingerprint +":"+cert.sha1Fingerprint;
-         var der = cert.getRawDER();
+       var chainArray = certchain.getChain();
 
-         dump(der);
+       dump("----------------------------------\n");
+
+       for(var i = 0; i < chainArray.length; i++) {
+         var cert = chainArray.queryElementAt(i, Ci.nsIX509Cert);
+         var fp = cert.md5Fingerprint +":"+cert.sha1Fingerprint;
+         var len = new Object();
+         var derData = cert.getRawDER(len);
+         dump("DerData: "+this.base64_encode(derData, false, false)+"\n");
 
          // XXX: Use an async XMLHTTPRequest:
          // XXX: Ask to submit cert
          // XXX: AS number??
        }
+       dump("----------------------------------\n");
      }
    }
   },
@@ -171,6 +177,65 @@ SSLObservatory.prototype = {
   // Hack to cause us to get instantiate early
   _xpcom_categories: [ { category: "profile-after-change" }, ],
 
+  encString: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/',
+  encStringS: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_',
+
+  base64_encode: function(inp, uc, safe) {
+    // do some argument checking
+    if (arguments.length < 1) return null;
+    var readBuf = new Array();    // read buffer
+    if (arguments.length >= 3 && safe != true && safe != false) return null;
+    var enc = (arguments.length >= 3 && safe) ? this.encStringS : this.encString; // character set used
+    var b = (typeof inp == "string"); // how input is to be processed
+    if (!b && (typeof inp != "object") && !(inp instanceof Array)) return null; // bad input
+    if (arguments.length < 2) {
+      uc = true;                  // set default
+    } // otherwise its value is passed from the caller
+    if (uc != true && uc != false) return null;
+    var n = (!b || !uc) ? 1 : 2;  // length of read buffer
+    var out = '';                 // output string
+    var c = 0;                    // holds character code (maybe 16 bit or 8 bit)
+    var j = 1;                    // sextett counter
+    var l = 0;                    // work buffer
+    var s = 0;                    // holds sextett
+
+    // convert  
+    for (var i = 0; i < inp.length; i++) {  // read input
+      c = (b) ? inp.charCodeAt(i) : inp[i]; // fill read buffer
+      for (var k = n - 1; k >= 0; k--) {
+        readBuf[k] = c & 0xff;
+        c >>= 8;
+      }
+      for (var m = 0; m < n; m++) {         // run through read buffer
+        // process bytes from read buffer
+        l = ((l<<8)&0xff00) | readBuf[m];   // shift remaining bits one byte to the left and append next byte
+        s = (0x3f<<(2*j)) & l;              // extract sextett from buffer
+        l -=s;                              // remove those bits from buffer;
+        out += enc.charAt(s>>(2*j));        // convert leftmost sextett and append it to output
+        j++;
+        if (j==4) {                         // another sextett is complete
+          out += enc.charAt(l&0x3f);        // convert and append it
+          j = 1;
+        }
+      }        
+    }
+    switch (j) {                            // handle left-over sextetts
+      case 2:
+        s = 0x3f & (16 * l);                // extract sextett from buffer
+        out += enc.charAt(s);               // convert leftmost sextett and append it to output
+        out += '==';                        // stuff
+        break;
+      case 3:
+        s = 0x3f & (4 * l);                 // extract sextett from buffer
+        out += enc.charAt(s);               // convert leftmost sextett and append it to output
+        out += '=';                         // stuff
+        break;
+      default:
+        break;
+    }
+
+    return out;
+  },
 
   log: function(level, str) {
     var econsole = Components.classes["@mozilla.org/consoleservice;1"]
