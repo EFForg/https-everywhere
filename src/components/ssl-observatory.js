b1819a2335b4a0380dc1133044a870c1afaeac38
Peter Eckersley <pde@eff.org>
2011-06-20 13:33:54 -0700

    Fix chrome.manifest for FF4

--- src/components/ssl-observatory.js
+++ src/components/ssl-observatory.js
@@ -25,6 +25,7 @@ const SERVICE_NAME = "Anonymously Submits strange SSL certificates to EFF.";
 const LOADER = CC["@mozilla.org/moz/jssubscript-loader;1"].getService(CI.mozIJSSubScriptLoader);
 
 const _INCLUDED = {};
+
 const INCLUDE = function(name) {
   if (arguments.length > 1)
     for (var j = 0, len = arguments.length; j < len; j++)
