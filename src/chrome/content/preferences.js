a947b1ab107e709eaba90950f5c9518201db928f
Kevin Jacobs <kevin.a.jacobs@hotmail.com>
2011-08-11 21:34:22 -0700

    Modified window_opener() to fix bug 3696 (bug applied to donation links in "About HTTPS Everywhere" as well).

--- src/chrome/content/preferences.js
+++ src/chrome/content/preferences.js
@@ -116,9 +116,15 @@ function https_prefs_init(doc) {
 
 function window_opener(uri) {
   // we don't use window.open, because we need to work around TorButton's state control
-  CC['@mozilla.org/appshell/window-mediator;1'].getService(CI.nsIWindowMediator)
-                                               .getMostRecentWindow('navigator:browser')
-                                               .open(uri, '','centerscreen' );
+    if(typeof gBrowser == "undefined"){
+        var window = CC["@mozilla.org/appshell/window-mediator;1"].getService(Components.interfaces.nsIWindowMediator);
+        var browserWindow = window.getMostRecentWindow("navigator:browser").getBrowser();
+        var newTab = browserWindow.addTab(uri, null, null);
+        browserWindow.selectedTab = newTab;
+
+    }
+    else
+        gBrowser.selectedTab = gBrowser.addTab(uri);
 }
 
 function https_prefs_accept() {
