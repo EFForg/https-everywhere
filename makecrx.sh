4e5690bcd497148084ccdaf5b2778d5b1b8c7efb
Seth Schoen <schoen@eff.org>
2012-02-21 12:48:39 -0800

    xmllint RELAX NG validation in crx build script too

--- makecrx.sh
+++ makecrx.sh
@@ -50,6 +50,19 @@ else
   exit 1
 fi
 
+if [ -f relaxng.xml -a -x "$(which xmllint)" ] >&2
+then
+  if xmllint --noout --relaxng relaxng.xml src/chrome/content/rules/*.xml
+  then
+    echo Validation of rulesets with RELAX NG grammar completed. >&2
+  else
+    echo ERROR: Validation of rulesets with RELAX NG grammar failed. >&2
+    exit 1
+  fi
+else
+  echo Validation of rulesets with RELAX NG grammar was SKIPPED. >&2
+fi
+
 sed -e "s/VERSION/$VERSION/g" chromium/updates-master.xml > chromium/updates.xml
 
 [ -d pkg ] || mkdir -p pkg
