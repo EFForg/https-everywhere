020b2e7ea02bd471432c266f405096b43bd6b997
graffatcolmingov <graffatcolmingov@gmail.com>
2012-05-08 17:04:58 -0400

    Prevent it from launching 3000+ processes at once.
    Use PDE's semantics here from tosback project.

--- trivial-response.py
+++ trivial-response.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import sys, os, re, subprocess
+from time import sleep
 
 try:
     from lxml import etree
@@ -17,31 +18,40 @@ report_file = '/'.join([base_dir, 'response_report.txt'])
 if sys.argv[1:]:
     os.chdir(sys.argv[1])
 
+failure = 0
 default_off = 0
 procs = []
+files = os.listdir('.')
+PARALLELISM = 50
 
 with open(report_file, 'w+') as fd:
     fd.truncate(0)
 
-for fil in os.listdir('.'):
-    if 'mismatches' in fil:
-        continue
-    try:
-        tree = etree.parse(fil)
-
-        if tree.xpath('/ruleset/@default_off'):
-            default_off += 1
+while True:
+    if files and len(procs) < PARALLELISM:
+        fil = files.pop()
+        if 'mismatches' in fil:
             continue
-    except Exception, e:
-        continue
+        try:
+            tree = etree.parse(fil)
 
-    proc = subprocess.Popen([rule_script, fil, report_file])
-    procs.append(proc)
+            if tree.xpath('/ruleset/@default_off'):
+                default_off += 1
+                continue
+        except Exception, e:
+            continue
 
-failure = 0
+        proc = subprocess.Popen([rule_script, fil, report_file])
+        procs.append(proc)
 
-for proc in procs:
-    failure = failure or proc.poll()
+    for proc in procs:
+        failure = failure or proc.poll()
+        procs.remove(proc)
+
+    if not (files or procs):
+        break
+
+    sleep(0.5)
 
 sys.stdout.write("Skipped %d default_off rulesets.\n" % default_off)
 
