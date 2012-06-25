48143444b5a3bdb32631be61465414340dee2daf
Peter Eckersley <pde@eff.org>
2012-06-25 16:24:51 -0700

    [Observatory] Switch base64 encoding from JS to native Mozilla
    https://trac.torproject.org/projects/tor/ticket/5894

--- src/components/ssl-observatory.js
+++ src/components/ssl-observatory.js
@@ -509,7 +509,10 @@ SSLObservatory.prototype = {
       var len = new Object();
       var derData = c.certArray[i].getRawDER(len);
       //var encoded = browserWindow.btoa(derData);  // seems to not be a real base 64 encoding!
-      base64Certs.push(this.base64_encode(derData, false, false));
+      let result = "";
+      for (let j = 0, dataLength = derData.length; j < dataLength; ++j) 
+        result += String.fromCharCode(derData[j]);
+      base64Certs.push(btoa(result));
     }
 
     var reqParams = [];
@@ -690,64 +693,7 @@ SSLObservatory.prototype = {
 
   encString: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/',
   encStringS: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_',
-
-  base64_encode: function(inp, uc, safe) {
-    // do some argument checking
-    if (arguments.length < 1) return null;
-    var readBuf = new Array();    // read buffer
-    if (arguments.length >= 3 && safe != true && safe != false) return null;
-    var enc = (arguments.length >= 3 && safe) ? this.encStringS : this.encString; // character set used
-    var b = (typeof inp == "string"); // how input is to be processed
-    if (!b && (typeof inp != "object") && !(inp instanceof Array)) return null; // bad input
-    if (arguments.length < 2) {
-      uc = true;                  // set default
-    } // otherwise its value is passed from the caller
-    if (uc != true && uc != false) return null;
-    var n = (!b || !uc) ? 1 : 2;  // length of read buffer
-    var out = '';                 // output string
-    var c = 0;                    // holds character code (maybe 16 bit or 8 bit)
-    var j = 1;                    // sextett counter
-    var l = 0;                    // work buffer
-    var s = 0;                    // holds sextett
-
-    // convert  
-    for (var i = 0; i < inp.length; i++) {  // read input
-      c = (b) ? inp.charCodeAt(i) : inp[i]; // fill read buffer
-      for (var k = n - 1; k >= 0; k--) {
-        readBuf[k] = c & 0xff;
-        c >>= 8;
-      }
-      for (var m = 0; m < n; m++) {         // run through read buffer
-        // process bytes from read buffer
-        l = ((l<<8)&0xff00) | readBuf[m];   // shift remaining bits one byte to the left and append next byte
-        s = (0x3f<<(2*j)) & l;              // extract sextett from buffer
-        l -=s;                              // remove those bits from buffer;
-        out += enc.charAt(s>>(2*j));        // convert leftmost sextett and append it to output
-        j++;
-        if (j==4) {                         // another sextett is complete
-          out += enc.charAt(l&0x3f);        // convert and append it
-          j = 1;
-        }
-      }        
-    }
-    switch (j) {                            // handle left-over sextetts
-      case 2:
-        s = 0x3f & (16 * l);                // extract sextett from buffer
-        out += enc.charAt(s);               // convert leftmost sextett and append it to output
-        out += '==';                        // stuff
-        break;
-      case 3:
-        s = 0x3f & (4 * l);                 // extract sextett from buffer
-        out += enc.charAt(s);               // convert leftmost sextett and append it to output
-        out += '=';                         // stuff
-        break;
-      default:
-        break;
-    }
-
-    return out;
-  },
-
+  
   log: function(level, str) {
     var econsole = CC["@mozilla.org/consoleservice;1"]
       .getService(CI.nsIConsoleService);
