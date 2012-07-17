5a13a7b217544b9c7052785ec1a699de1700046c
Peter Eckersley <pde@eff.org>
2012-07-16 17:25:19 -0700

    Graceful handling of unfindable ApplicableList objects
    https://trac.torproject.org/projects/tor/ticket/6280

--- src/components/https-everywhere.js
+++ src/components/https-everywhere.js
@@ -430,7 +430,7 @@ HTTPSEverywhere.prototype = {
       var lst = this.getApplicableListForChannel(channel);
       if (channel.URI.spec in https_everywhere_blacklist) {
         this.log(DBUG, "Avoiding blacklisted " + channel.URI.spec);
-        lst.breaking_rule(https_everywhere_blacklist[channel.URI.spec]);
+        if (lst) lst.breaking_rule(https_everywhere_blacklist[channel.URI.spec]);
         return;
       }
       HTTPS.replaceChannel(lst, channel);
