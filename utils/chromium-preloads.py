29a3304e083f1a64f2d933d5a866044b58a9f8e1
Yan <yan@mit.edu>
2013-06-17 12:43:28 -0700

    Fix encoding bug in chromium-preloads.py
    When using urllib.request in Python 3, the output needs to be
    converted from bytes to string for it to be parsed in chromium-
    preloads.py.

--- utils/chromium-preloads.py
+++ utils/chromium-preloads.py
@@ -5,41 +5,42 @@
 
 import urllib.request, urllib.error, urllib.parse, re, glob, os
 preloads = urllib.request.urlopen("https://src.chromium.org/viewvc/chrome/trunk/src/net/http/transport_security_state_static.h?content-type=text%2Fplain").read()
+preloads = preloads.decode('utf-8')
 
 def escape(s):
     return re.sub("\.", "\\.", s)
 
 def make_rule(name, hosts):
     output = """<!-- This rule was automatically generated based on an HSTS
-     preload rule in the Chromium browser.  See 
+     preload rule in the Chromium browser.  See
      https://src.chromium.org/viewvc/chrome/trunk/src/net/http/transport_security_state_static.h
      for the list of preloads.  Sites are added to the Chromium HSTS
      preload list on request from their administrators, so HTTPS should
      work properly everywhere on this site.
- 
+
      Because Chromium and derived browsers automatically force HTTPS for
      every access to this site, this rule applies only to Firefox. -->\n"""
     output += '<ruleset name="%s" platform="firefox">\n' % name.capitalize()
     for h in hosts:
         output += '<target host="%s" />\n' % h
-    
+
     output += "\n"
-    
+
     for h in hosts:
         output += '<securecookie host="^%s$" name=".+" />\n' % escape(h)
-    
+
     output += "\n"
-        
+
     for h in hosts:
         output += '<rule from="^http://%s/" to="https://%s/" />\n' % (escape(h), h)
-        
+
     output += "</ruleset>\n"
     open("from-preloads/%s.xml" % name.capitalize(), "w").write(output)
 
-t = re.compile('", true')
+t = re.compile('&quot;, true')
 preloads = list(filter(t.search,preloads.split("\n")))
 
-preloads = [x.split('"')[1] for x in preloads]
+preloads = [x.split('&quot;')[1] for x in preloads]
 preloads = [re.sub('\\\\[0-9]*', '.', x) for x in preloads]
 preloads = [re.sub('^\.', '', x) for x in preloads]
 
@@ -52,7 +53,7 @@ for x in preloads:
         continue
     domain = ".".join(x.split(".")[-2:])
     d.setdefault(domain, []).append(x)
-        
+
 if not os.access("from-preloads", 0):
     os.mkdir("from-preloads")
 
