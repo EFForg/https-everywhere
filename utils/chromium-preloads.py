4bf0eda18f73e5864c4ad373d00c4a0f6ede1369
Yan <yan@mit.edu>
2013-06-17 17:46:50 -0700

    Be more careful about finding HSTS duplicate rules
    chromium-preloads.py needs to check whether a rule in the hsts preload
    list applies to a target already in the https-e rules, and, if so,
    ignores the hsts rule. Previously this was done by matching the hsts
    rule name (ex: google.com) anywhere in the content of the https-e
    xml rule files. This commit changes the script to find exact matches between
    hsts rule names and https-e targets, properly handling wildcards.
    
    Before this change, chromium-preloads.py created 62 new rules. After,
    it created 48.

--- utils/chromium-preloads.py
+++ utils/chromium-preloads.py
@@ -4,11 +4,39 @@
 # preload list (in the from-preloads/ directory)
 
 import urllib.request, urllib.error, urllib.parse, re, glob, os
+from lxml import etree
+
 preloads = urllib.request.urlopen("https://src.chromium.org/viewvc/chrome/trunk/src/net/http/transport_security_state_static.h?content-type=text%2Fplain").read()
 preloads = preloads.decode('utf-8')
 
 def escape(s):
     return re.sub("\.", "\\.", s)
+
+def get_targets():
+    """extract unique targets from the set of https-e rulesets"""
+    ruledir = "src/chrome/content/rules/"
+    rules = [ruledir+fi for fi in os.listdir(ruledir) if fi.endswith(".xml")]
+    targets = set()
+    for fi in rules:
+        tree = etree.parse(fi)
+        targets.update(tree.xpath("/ruleset/target/@host"))
+    return targets
+
+def is_dup(rule, targets):
+    """find if a rule is duplicated in the existing https-e rulesets"""
+    if rule.lower() in map(lambda x: x.lower(), targets):
+        return True
+    else:
+        wildcards = filter(lambda x: '*' in x, targets)
+        for url in wildcards:
+            url = re.escape(url)
+            url = re.sub(r'\\\*', '.*', url)  # handle wildcards
+            url = '^'+url+'$'
+            if re.compile(url, re.I).search(rule):
+                return True
+            else:
+                continue
+        return False
 
 def make_rule(name, hosts):
     output = """<!-- This rule was automatically generated based on an HSTS
@@ -44,11 +72,15 @@ preloads = [x.split('&quot;')[1] for x in preloads]
 preloads = [re.sub('\\\\[0-9]*', '.', x) for x in preloads]
 preloads = [re.sub('^\.', '', x) for x in preloads]
 
-rules = [open(x).read() for x in glob.glob("src/chrome/content/rules/*.xml")]
+print("Found %d rules total in HSTS preloads list" % len(preloads))
+
+#rules = [open(x).read() for x in glob.glob("src/chrome/content/rules/*.xml")]
+
+targets = get_targets()
 
 d = {}
 for x in preloads:
-    if any(map(re.compile(x).search, rules)):
+    if is_dup(x, targets):
         print("Ignored existing domain", x)
         continue
     domain = ".".join(x.split(".")[-2:])
