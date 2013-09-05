5d2e09f74ba0a7dda3aa86e04399cad27f9f8c07
Yan <yan@mit.edu>
2013-09-05 13:17:45 -0700

    Create zip info based on pre-generated zip
    Before, when I tried constructing the zip metadata from scratch,
    FF would give a corrupt file error when trying to load the xpi.

--- utils/create_xpi.py
+++ utils/create_xpi.py
@@ -7,6 +7,7 @@ import glob
 xpiName = sys.argv[1]
 exclusionsFile = sys.argv[2]
 exclusions = []
+tmpfile = '../pkg/tmp.xpi'
 
 with open(exclusionsFile) as f:
     for line in f:
@@ -14,29 +15,34 @@ with open(exclusionsFile) as f:
 
 xpiFile = zipfile.ZipFile(xpiName, mode='w')
 
-def constructZipInfo(filename):
-    """Create deterministic ZIP metadata for a given file"""
-    info = zipfile.ZipInfo(filename, time.gmtime(1378343307))
-    info.compress_type = 0
-    info.comment = ''
-    info.extra = ''
-    info.create_system = 3
-    info.create_version = 20
-    info.extract_version = 20
-    if os.path.isfile(filename):
-        info.external_attr = 0664 << 16L
-    elif os.path.isdir(filename):
-        info.external_attr = 04775 << 16L
-    # info.external_attr = 2176057344L
-    info.volume = 0
-    return info
+def createTmpZipInfo():
+    """
+    Create a non-deterministic zip in order to use the file info
+    generated to create a deterministic zip
+    """
+    xpiFileTmp = zipfile.ZipFile(tmpfile, mode='w')
+    for root,subfolders,files in os.walk('.'):
+        for fi in files:
+            filename = os.path.join(root,fi)
+            if filename not in map(lambda x: './'+x, exclusions):
+                xpiFileTmp.write(filename)
+    xpiFileTmp.close()
+    return xpiFileTmp.infolist()
 
-for root,subfolders,files in os.walk('.'):
-    for fi in files:
-        filename = os.path.join(root,fi)
-        if filename not in map(lambda x: './'+x, exclusions):
-            print("Adding to zip: "+filename)
-            info = constructZipInfo(filename)
-            xpiFile.writestr(info, open(filename).read())
+def constructZipDet():
+    """
+    Create a deterministic zip by setting timestamps and
+    system/version info to hard-coded values
+    """
+    tmpInfo = createTmpZipInfo()
+    for info in tmpInfo:
+        info.date_time = time.gmtime(1378343307)
+        info.create_system = 3
+        info.create_version = 20
+        info.extract_version = 20
+        print("adding to zip: "+info.filename)
+        xpiFile.writestr(info, open(info.filename).read())
 
+constructZipDet()
 xpiFile.close()
+os.remove(tmpfile)
