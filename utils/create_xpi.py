2a875b2685ba8d7939b6a07e07d6ab397214bec1
Yan <yan@mit.edu>
2013-09-05 09:42:32 -0700

    Fix build exclusions

--- utils/create_xpi.py
+++ utils/create_xpi.py
@@ -11,13 +11,15 @@ exclusions = []
 with open(exclusionsFile) as f:
     for line in f:
         exclusions.extend(glob.glob(line.strip()))
+print(exclusions)
 
 xpiFile = zipfile.ZipFile(xpiName, mode='w')
 
 for root,subfolders,files in os.walk('.'):
     for fi in files:
         filename = os.path.join(root,fi)
-        if filename not in exclusions:
+        if filename not in map(lambda x: './'+x, exclusions):
+          print(filename)
           info = zipfile.ZipInfo(filename, time.gmtime(1378343307))
           xpiFile.writestr(info, '')
 
