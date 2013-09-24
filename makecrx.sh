24e234ac739fb8085808e4d8752ee72fcceda270
Yan <yan@mit.edu>
2013-09-24 16:39:30 -0700

    sha1sum doesn't work on OS X

--- makecrx.sh
+++ makecrx.sh
@@ -117,7 +117,7 @@ trap 'rm -f "$pub" "$sig" "$zip"' EXIT
 # zip up the crx dir
 cwd=$(pwd -P)
 (cd "$dir" && python ../../utils/create_xpi.py -n "$cwd/$zip" .)
-echo >&2 "Unsigned package has sha1sum: `sha1sum "$cwd/$zip"`" 
+#echo >&2 "Unsigned package has sha1sum: `sha1sum "$cwd/$zip"`" 
 
 # signature
 openssl sha1 -sha1 -binary -sign "$key" < "$zip" > "$sig"
