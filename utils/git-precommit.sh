50968d60df05f89c2e51d790732312de052ee959
Jonathan Davies <jpdavs@gmail.com>
2013-10-11 08:35:44 +0000

    utils/git-precommit.sh: Only run tests if changes have been made to our rulesets.

--- utils/git-precommit.sh
+++ utils/git-precommit.sh
@@ -8,12 +8,19 @@
 echo "$(date -R): Stashing unrelated changes in Git..."
 git stash -q --keep-index
 
-echo "$(date -R): Running tests:"
-./utils/trivial-validate.py src/chrome/content/rules > /dev/null
-RESULT=$?
+# Only run tests if our rulesets have been changed.
+RULESET_PATTERN="src/chrome/content/rules/"
+if [ ! $(git diff --cached --name-only | grep "$RULESET_PATTERN") ]; then
+    echo "$(date -R): Skipping tests, no changes in $RULESET_PATTERN."
+    exit 0
+else
+    echo "$(date -R): Running tests:"
+    ./utils/trivial-validate.py src/chrome/content/rules > /dev/null
+    RESULT=$?
 
-if [ $RESULT -eq 1 ]; then
-    echo "$(date -R): Failure encountered during ruleset validation."
+    if [ $RESULT -eq 1 ]; then
+        echo "$(date -R): Failure encountered during ruleset validation."
+    fi
 fi
 
 echo "$(date -R): Reverting Git stash..."
