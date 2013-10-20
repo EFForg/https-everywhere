79d599c831b11051578265874e7b77bc30f4d4df
Jonathan Davies <jpdavs@gmail.com>
2013-10-20 19:12:02 +0000

    create_xpi.py: Modified to use argparse instead of getopt module.

--- utils/create_xpi.py
+++ utils/create_xpi.py
@@ -7,29 +7,36 @@
 Usage: python create_xpi.py -x <exclusions> -n <name of zipped file> <directory>
 """
 
+import argparse
 import zipfile_deterministic as zipfile
 import sys
 import glob
-import getopt
 
-opts, args = getopt.getopt(sys.argv[1:], 'x:n:')
+parser = argparse.ArgumentParser(
+    description="Deterministic XPI file creator.")
+parser.add_argument("-x", type=str, nargs="?",
+    dest="exclusions", metavar="File with file pattern exclusions",
+    default=".build_exclusions", help="Excluded file patterns.")
+parser.add_argument("-n", type=str,
+    dest="name", help="Name of Zip file.")
+parser.add_argument("directory", type=str,
+    help="Directory to compress.")
+
+args = parser.parse_args()
+
+xpiName = args.name
+exclusionsFile = args.exclusions
+directory = args.directory
 
 exclusions = []
-for o, v in opts:
-    if o == "-x":
-        exclusionsFile = v
-        with open(exclusionsFile) as f:
-            for line in f:
-                exclusions.extend(glob.glob(line.strip()))
-        exclusions = map(lambda x: './'+x, exclusions)
-    elif o == "-n":
-        xpiName = v
+with open(exclusionsFile) as f:
+    for line in f:
+        exclusions.extend(glob.glob(line.strip()))
+exclusions = map(lambda x: './'+x, exclusions)
 
 compress = zipfile.ZIP_DEFLATED
 
 xpiFile = zipfile.ZipFile(xpiName, mode='w', compression=compress)
-
-directory = args[0]
 
 xpiFile.write_from_directory(directory, exclusions, compress_type=compress)
 xpiFile.close()
