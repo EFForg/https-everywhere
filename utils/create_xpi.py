d9684ca513ace131fc77be0ef16fd3fc208da3fd
Jonathan Davies <jpdavs@gmail.com>
2013-10-21 08:32:06 +0000

    create_xpi.py: Set to use argparse variables directly instead of using separate ones.

--- utils/create_xpi.py
+++ utils/create_xpi.py
@@ -18,25 +18,21 @@ parser.add_argument("-x", type=str, nargs="?",
     dest="exclusions", metavar="File with file pattern exclusions",
     default=".build_exclusions", help="Excluded file patterns.")
 parser.add_argument("-n", type=str,
-    dest="name", help="Name of Zip file.")
+    dest="xpiname", help="Name of target XPI file.")
 parser.add_argument("directory", type=str,
     help="Directory to compress.")
 
 args = parser.parse_args()
 
-xpiName = args.name
-exclusionsFile = args.exclusions
-directory = args.directory
-
 exclusions = []
-with open(exclusionsFile) as f:
+with open(args.exclusions) as f:
     for line in f:
         exclusions.extend(glob.glob(line.strip()))
 exclusions = map(lambda x: './'+x, exclusions)
 
 compress = zipfile.ZIP_DEFLATED
 
-xpiFile = zipfile.ZipFile(xpiName, mode='w', compression=compress)
+xpiFile = zipfile.ZipFile(args.xpiname, mode='w', compression=compress)
 
-xpiFile.write_from_directory(directory, exclusions, compress_type=compress)
+xpiFile.write_from_directory(args.directory, exclusions, compress_type=compress)
 xpiFile.close()
