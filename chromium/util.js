03dd5338972da937fa0d0445fae1314d015630aa
Nick Semenkovich <semenko@alum.mit.edu>
2013-12-21 12:11:32 -0600

    Let developers know about DEFAULT_LOG_LEVEL.
    Since we are much more silent, let anyone who opens the console know that
    DEFAULT_LOG_LEVEL can be adjusted to debug rules, etc.
    
    Signed-off-by: Nick Semenkovich <semenko@alum.mit.edu>

--- chromium/util.js
+++ chromium/util.js
@@ -9,6 +9,9 @@ WARN=5;
 // TODO: Add an easy UI to change the log level.
 // (Developers can just type DEFAULT_LOG_LEVEL=1 in the console)
 DEFAULT_LOG_LEVEL=4;
+console.log("Hey developer! Want to see more verbose logging?");
+console.log("Type this into the console: DEFAULT_LOG_LEVEL=1");
+
 function log(level, str) {
     if (level >= DEFAULT_LOG_LEVEL) {
         if (level === WARN) {
