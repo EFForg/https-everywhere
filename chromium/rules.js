43ad925ba4ffbf1ffb7b77dde0f10a176ca61639
Nick Semenkovich <semenko@alum.mit.edu>
2013-12-20 22:02:02 -0600

    Clean up some tiny but annoying lint warnings
    Some unused variables (and some forgotten ones).
    
    Signed-off-by: Nick Semenkovich <semenko@alum.mit.edu>
    
    Conflicts:
    
    	chromium/rules.js

--- chromium/rules.js
+++ chromium/rules.js
@@ -189,7 +189,7 @@ RuleSets.prototype = {
     }
     log(DBUG, "Ruleset cache miss for " + host);
 
-    var i, tmp, t;
+    var tmp, t;
     var results = this.global_rulesets.slice(0); // copy global_rulesets
     if (this.targets[host])
       results = results.concat(this.targets[host]);
@@ -229,7 +229,6 @@ RuleSets.prototype = {
     //log(DBUG, "  host: " + cookie.host);
     //log(DBUG, "  domain: " + cookie.domain);
     //log(DBUG, "  rawhost: " + cookie.rawHost);
-    var i,j;
     var hostname = cookie.domain;
     // cookie domain scopes can start with .
     while (hostname.charAt(0) == ".")
@@ -302,10 +301,9 @@ RuleSets.prototype = {
   },
 
   rewriteURI: function(urispec, host) {
-    var i = 0;
     var newuri = null;
     var rs = this.potentiallyApplicableRulesets(host);
-    for(i = 0; i < rs.length; ++i) {
+    for(var i = 0; i < rs.length; ++i) {
       if (rs[i].active && (newuri = rs[i].apply(urispec)))
         return newuri;
     }
