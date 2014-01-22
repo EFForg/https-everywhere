40c48d9d986b12ccbc33379e3f0d0d21f666a744
Yan <yan@mit.edu>
2014-01-22 14:16:28 -0800

    Linting

--- src/chrome/content/code/ApplicableList.js
+++ src/chrome/content/code/ApplicableList.js
@@ -2,16 +2,16 @@
 // were applied, and which ones weren't but might have been, to the contents
 // of a given page (top level nsIDOMWindow)
 
-serial_number = 0
+var serial_number = 0;
 
 function ApplicableList(logger, doc, domWin) {
   this.domWin = domWin;
   this.uri = doc.baseURIObject.clone();
   if (!this.uri) {
     this.log(WARN,"NULL CLONING URI " + doc);
-    if (doc) 
+    if (doc)
       this.log(WARN,"NULL CLONING URI " + doc.baseURIObject);
-    if (doc.baseURIObject) 
+    if (doc.baseURIObject)
       this.log(WARN,"NULL CLONING URI " + doc.baseURIObject.spec);
   }
   this.home = doc.baseURIObject.spec; // what doc we're housekeeping for
@@ -19,8 +19,8 @@ function ApplicableList(logger, doc, domWin) {
   this.active = {};
   this.breaking = {}; // rulesets with redirection loops
   this.inactive = {};
-  this.moot={};  // rulesets that might be applicable but uris are already https
-  this.all={};  // active + breaking + inactive + moot
+  this.moot = {};  // rulesets that might be applicable but uris are already https
+  this.all = {};  // active + breaking + inactive + moot
   serial_number += 1;
   this.serial = serial_number;
   this.log(DBUG,"Alist serial #" + this.serial + " for " + this.home);
@@ -32,10 +32,10 @@ ApplicableList.prototype = {
     // Empty everything, used when toggles occur in order to ensure that if
     // the reload fails, the resulting list is not eroneous
     this.active = {};
-    this.breaking = {}; 
+    this.breaking = {};
     this.inactive = {};
-    this.moot={};  
-    this.all={};  
+    this.moot = {};
+    this.all = {};
   },
 
   active_rule: function(ruleset) {
@@ -53,7 +53,6 @@ ApplicableList.prototype = {
   },
 
   inactive_rule: function(ruleset) {
-
     this.log(INFO,"inactive rule " + ruleset.name +" in "+ this.home +" -> " +
              this.domWin.document.baseURIObject.spec+ " serial " + this.serial);
     this.inactive[ruleset.name] = ruleset;
