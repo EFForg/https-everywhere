d03e1bfc48833fc144418d742cf336f7be8e414e
redwire <redwire@riseup.net>
2014-06-30 12:43:04 -0700

    Add commas to key-value pair separator

--- utils/ruleset_update_manifest.py
+++ utils/ruleset_update_manifest.py
@@ -105,7 +105,7 @@ while update['hash'] is None:
 while not done:
     file_name = input("Where should the JSON contents be stored? ")
     try:
-        json_data = json.dumps(update, sort_keys=True).replace(', ', '\n')
+        json_data = json.dumps(update, sort_keys=True).replace(', ', '\n,')
         open(file_name, 'w').write(json_data)
         print("The update contents have been successfully written.")
         done = True
