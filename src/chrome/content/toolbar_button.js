27119a135747d6983e7da8ab1942e23fbeecad53
Yan Zhu <yan@mit.edu>
2014-07-24 16:58:51 -0700

    Revert "Disable HTTP Nowhere menu item when HTTPS Everywhere is disabled"
    This reverts commit bda2202847abd0e7a71febd566b74509644eeed3.

--- src/chrome/content/toolbar_button.js
+++ src/chrome/content/toolbar_button.js
@@ -73,8 +73,6 @@ httpsEverywhere.toolbarButton = {
     httpNowhereItem.setAttribute('checked', showHttpNowhere ? 'true' : 'false');
     toolbarbutton.setAttribute('http_nowhere',
                                showHttpNowhere ? 'true' : 'false');
-    var enabled = HTTPSEverywhere.prefs.getBoolPref("globalEnabled");
-    httpNowhereItem.setAttribute('disabled', enabled ? 'false' : 'true');
 
     // show ruleset counter when a tab is changed
     tb.updateRulesetsApplied();
@@ -230,11 +228,9 @@ httpsEverywhere.toolbarButton = {
    * Toggles whether HTTP Nowhere mode is active, updates the toolbar icon.
    */
   toggleHttpNowhere: function() {
-    HTTPSEverywhere.toggleHttpNowhere();
     var tb = httpsEverywhere.toolbarButton;
+    HTTPSEverywhere.toggleHttpNowhere();
     var showHttpNowhere = tb.shouldShowHttpNowhere();
-
-    // Change icon color to red if HTTP nowhere is enabled
     var toolbarbutton = document.getElementById('https-everywhere-button');
     toolbarbutton.setAttribute('http_nowhere',
                                showHttpNowhere ? 'true' : 'false');
@@ -356,11 +352,6 @@ function reload_window() {
 function toggleEnabledState(){
 	HTTPSEverywhere.toggleEnabledState();
 	reload_window();	
-
-  // Disable/enable toggling HTTP Nowhere mode
-  var httpNowhereItem = document.getElementById('http-nowhere-item');
-  var enabled = HTTPSEverywhere.prefs.getBoolPref("globalEnabled");
-  httpNowhereItem.setAttribute('disabled', enabled ? 'false' : 'true');
 
   // Change icon depending on enabled state
   httpsEverywhere.toolbarButton.changeIcon();
