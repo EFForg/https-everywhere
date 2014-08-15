bc259556ef8091478b1f521ad870874413ac0d19
redwire <redwire@riseup.net>
2014-08-14 22:46:20 -0230

    Clean up my code and rewrite better comments

--- src/chrome/content/code/rulesetUpdate.js
+++ src/chrome/content/code/rulesetUpdate.js
@@ -15,16 +15,10 @@
 // TODO
 // Set this value.
 /* Hardcoded public key used to verify the signature over the update data */
-const RULESET_UPDATE_KEY = ''+
-  'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA7wJz/Ekn4loB+GX/TnObTo/5J0/aq1hBl'+
-  '+xeSyCUX/fggjju5jnRnbnQx10OaZ655Yft4Cs2IfdIh95NYsN+gfi6HVesy/Q9G72BjhpW6+gTlk'+
-  'W9vW56xwjv+Cpi5/20SKbvMZCMXTvR50HqLaLiOeLyAOQv06FKlyF5kbgQwpayExii75KFJL3HlH5'+
-  '+mZfNfKElNK9Oyiig7sqnVTOdovNCFnW8zom2fS3YyODaFvPUSmo1Yd7Mr0xWjE5rAV7k70aZlR1N'+
-  'Eze/Tfcf42LEhY5XkflczIWh+cse/v/sbZadS9jxbD2SgEJuLatF5zupmd0acvj1II8do2RE95FQC'+
-  'QIDAQAB';
+const RULESET_UPDATE_KEY = '';
 
 /* extension release branch preference key */
-const BRANCH_PREF= 'extensions.https_everywhere.branch_name';
+const BRANCH_PREF = 'extensions.https_everywhere.branch_name';
 
 /* extension release version preference key */
 const VERSION_PREF = 'extensions.https_everywhere.release_version';
@@ -43,35 +37,23 @@ const TMP_RULESET_DBFILE_PATH = OS.Path.join(OS.Constants.Path.tmpDir,
 /* maximum number of attempts to fetch ruleset updates */
 const MAX_RSUPDATE_FETCHES = 6;
 
-/* name of the hash function to use to compute the digest of update.json content */
-const SIGNING_DIGEST_FN = 'sha256';
-
 const _prefs = CC["@mozilla.org/preferences-service;1"]
                  .getService(CI.nsIPrefService).getBranch("");
 
 /*******************************************************************************
  *** Design Explanation                                                        *
- * We can't write RulesetUpdater the usual way, with a constructor and then    *
- * by setting methods on the object's prototype, because we need access to     *
- * the HTTPSEverywhere object; trying to refer to it from RulesetUpdater's     *
- * "constructor" function results in cyclic dependencies since HTTPSEverywhere *
- * also needs a reference to RulesetUpdater to call fetchUpdate.               *
- *                                                                             *
- * We are thus forced to write RulesetUpdater as an object literal, but        *
- * doing so forces us to define methods in the reverse order from which        *
- * they are called. Rather than structuring my codebase backwards, and         *
- * since only the fetchUpdate function really needs to be exposed, I am        *
- * taking the approach you see here of defining everything I need inside       *
- * a closure, returning only the reference to fetchUpdate.                     *
+ * HTTPSEverywhere cannot be referred to within the constructor of a more      *
+ * typical object definition, because doing so would create a circular         *
+ * dependency between HTTPSEverywhere's and RulesetUpdater's constructors.     *
+ * Using an anonymous function, I create a new scope within which helper       *
+ * functions can be defined and invoked in a natural order.                    *
  *******************************************************************************
  */
 
 const RulesetUpdater = {     // BEGIN OBJECT DEFINITION
 fetch_update : (function() { // BEGIN MODULE
 
-/* Initiates the check for updates and tests of authenticity.
- * Must be wrapped in a function to call from setInterval, i.e.:
- * setInterval(function() { updater.fetchUpdate(); }, interval);
+/* Initiates the check for ruleset updates.
  */
 function fetchUpdate() {
   https_everywhereLog(INFO, "Calling fetchUpdate");
@@ -83,16 +65,15 @@ function fetchUpdate() {
     });
 }
 
-/* Verifies the signature on the updateObj.update and then issues a request that
- * will fetch and test the hash on the newly released ruleset database file.
- * updateObj - The JSON manifest of the update information for the ruleset update.
+/* Ensures that the ruleset update described by the downloaded update.json file is
+ * appropriate for this version of the extension and is new, verifies update.json's
+ * authenticity, and fetches the database file containing the updated rulesets.
+ *
+ * @param update - The string contents of the update.json file.
  */
 function conditionallyApplyUpdate(update) {
   https_everywhereLog(INFO, "Got update data:");
   https_everywhereLog(INFO, update);
-  https_everywhereLog(INFO, '' + update.charCodeAt(update.length - 3) + 
-                                 update.charCodeAt(update.length - 2) +
-                                 update.charCodeAt(update.length - 1));
   var updateObj = JSON.parse(update);
   var extVersion = _prefs.getCharPref(VERSION_PREF);
   var extBranch = _prefs.getCharPref(BRANCH_PREF);
@@ -124,6 +105,10 @@ function conditionallyApplyUpdate(update) {
 
 /* Attempts to verify the provided signature over updateStr using
  * the hardcoded RULESET_UPDATE_KEY public key.
+ *
+ * @param updateStr - The string contents of the downloaded update.json file.
+ * @param signature - The string contents of update.json.sig.
+ * @return bool     - true if the update.json content is authentic else false.
  */
 function verifyUpdateSignature(updateStr, signature) {
   var verifier = Cc['@mozilla.org/security/datasignatureverifier;1']
@@ -134,7 +119,12 @@ function verifyUpdateSignature(updateStr, signature) {
 
 
 /* Checks that the ruleset version to download is greater than the current ruleset library
- * version (rsVersion) and is a subversion of the extension version (extVersion).
+ * version  and is a subversion of the extension version.
+ *
+ * @param extVersion - The version of the extension (eg: "5.0.0")
+ * @param rsVersion  - The current ruleset version (eg: "5.0.0.1")
+ * @param newVersion - The version of the new ruleset supplied by update.json.
+ * @return bool      - true if the new ruleset version is applicable else false.
  */
 function checkVersionRequirements(extVersion, rsVersion, newVersion) {
   var verCompare = Cc['@mozilla.org/xpcom/version-comparator;1']
@@ -147,6 +137,13 @@ function checkVersionRequirements(extVersion, rsVersion, newVersion) {
   return sameExtVer && newRSVer;
 }
 
+/* Compute the hash of the contents of a binary file's contents.
+ *
+ * @param path    - The string path to the file whose contents are to be hashed.
+ * @param length  - The number of bytes of data in the file to hash.
+ * @param hashfn  - The name of the hash function to use (eg: "sha256").
+ * @return string - The hex-encoded hash of the file's contents.
+ */
 function hashBinaryFile(path, length, hashfn) {
   var f = Cc['@mozilla.org/file/local;1'].createInstance(Ci.nsILocalFile);
   var istream = Cc['@mozilla.org/network/file-input-stream;1']
@@ -171,10 +168,13 @@ function hashBinaryFile(path, length, hashfn) {
   return [toHexStr(hash.charCodeAt(i)) for (i in hash)].join('');
 }
 
-/* Issues a request to download a new, zipped ruleset database file and then determines whether
- * its hash matches the one provided in the verified update manifest before applying the changes.
- * url  - The full URL to fetch the file from, MUST be using HTTPS!
- * hash - The hash of the database file provided by the update manifest verified previously.
+/* Fetches the contents of the new ruleset database file, ensures that the contents' hash
+ * matches what update.json says it should be, and then makes the call to apply the new
+ * rulesets database.
+ *
+ * @param url    - The URL from which to fetch the new rulesets database file.
+ * @param hashfn - The name of the hash function to use when hashingthe database file.
+ * @param hash   - The hash provided by update.json.
  */
 function fetchRulesetDBFile(url, hashfn, hash) {
   https_everywhereLog(INFO, "Making request to get database file at " + url);
@@ -213,8 +213,8 @@ function fetchRulesetDBFile(url, hashfn, hash) {
   xhr.send(null);
 }
 
-/* Applies the new ruleset database file by replacing the old one and reinitializing 
- * the mapping of targets to applicable rules.
+/* Moves the downloaded rulesets database into a permanent location and reinitializes
+ * HTTPSRules to use the rulesets.
  */
 function applyNewRuleset() {
   https_everywhereLog(INFO, 'In applyNewRuleset');
