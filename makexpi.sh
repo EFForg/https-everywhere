2ad53065acebc99ce7a916d5afabd70b841cb2d9
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2014-08-29 18:41:16 -0400

    Speed up builds.
    Only build / validate rulesets when they have changed.
    
    Also break out validation steps into their own script, shared by
    makecrx/makexpi.

--- makexpi.sh
+++ makexpi.sh
@@ -16,6 +16,7 @@ APP_NAME=https-everywhere
 #  ./makexpi.sh 0.2.3.development.2
 
 cd "`dirname $0`"
+RULESETS_UNVALIDATED="$PWD/pkg/rulesets.unvalidated.sqlite"
 RULESETS_SQLITE="$PWD/src/defaults/rulesets.sqlite"
 ANDROID_APP_ID=org.mozilla.firefox
 
@@ -51,7 +52,13 @@ if [ -n "$1" ] && [ "$2" != "--no-recurse" ] && [ "$1" != "--fast" ] ; then
   exit 0
 fi
 
-if [ "$1" != "--fast" -o ! -f "$RULESETS_SQLITE" ] ; then
+# Only generate the sqlite database if any rulesets have changed. Tried
+# implementing this with make, but make is very slow with 11k+ input files.
+needs_update() {
+  find src/chrome/content/rules/ -newer $RULESETS_UNVALIDATED |\
+    grep -q .
+}
+if [ ! -f "$RULESETS_UNVALIDATED" ] || needs_update ; then
   # This is an optimization to get the OS reading the rulesets into RAM ASAP;
   # it's useful on machines with slow disk seek times; doing several of these
   # at once allows the IO subsystem to seek more efficiently.
@@ -59,7 +66,6 @@ if [ "$1" != "--fast" -o ! -f "$RULESETS_SQLITE" ] ; then
     # Those cover everything but it wouldn't matter if they didn't
     nohup cat src/chrome/content/rules/"$firstchar"*.xml >/dev/null 2>/dev/null &
   done
-
   echo "Generating sqlite DB"
   python2.7 ./utils/make-sqlite.py
 fi
@@ -72,51 +78,11 @@ die() {
   exit 1
 }
 
-if [ "$1" != "--fast" -a -z "$FAST" ] ; then
-  if python2.7 ./utils/trivial-validate.py --quiet --db $RULESETS_SQLITE >&2
-  then
-    echo Validation of included rulesets completed. >&2
-    echo >&2
-  else
-    die "Validation of rulesets failed."
-  fi
-
-  # Check for xmllint.
-  type xmllint >/dev/null || die "xmllint not available"
-
-  GRAMMAR="utils/relaxng.xml"
-  if [ -f "$GRAMMAR" ]
-  then
-    # xmllint spams stderr with "<FILENAME> validates, even with the --noout
-    # flag. We can't grep -v for that line, because the pipeline will mask error
-    # status from xmllint. Instead we run it once going to /dev/null, and if
-    # there's an error run it again, showing only error output.
-    validate_grammar() {
-      find src/chrome/content/rules -name "*.xml" | \
-       xargs xmllint --noout --relaxng utils/relaxng.xml
-    }
-    if validate_grammar 2>/dev/null
-    then
-      echo Validation of rulesets against $GRAMMAR succeeded. >&2
-    else
-      validate_grammar 2>&1 | grep -v validates
-      die "Validation of rulesets against $GRAMMAR failed."
-    fi
-  else
-    echo Validation of rulesets against $GRAMMAR SKIPPED. >&2
-  fi
-
-  if [ -x ./utils/compare-locales.sh ] >&2
-  then
-    if sh ./utils/compare-locales.sh >&2
-    then
-      echo Validation of included locales completed. >&2
-    else
-      die "Validation of locales failed."
-    fi
-  fi
+# If the unvalidated rulesets have changed, validate and copy to the validated
+# rulesets file.
+if [ "$RULESETS_UNVALIDATED" -nt "$RULESETS_SQLITE" ] ; then
+  bash utils/validate.sh
 fi
-# =============== END VALIDATION ================
 
 # The name/version of the XPI we're building comes from src/install.rdf
 XPI_NAME="pkg/$APP_NAME-`grep em:version src/install.rdf | sed -e 's/[<>]/	/g' | cut -f3`"
