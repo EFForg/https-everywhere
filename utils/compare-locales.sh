99f27d9ba282b8010506bdbe782e392007be9719
MB <kunjurcca@gmail.com>
2014-10-07 19:13:00 +0000

    [compare-locales] Cosmetics

--- utils/compare-locales.sh
+++ utils/compare-locales.sh
@@ -5,25 +5,24 @@ set -eu;
 # Check whether all included rules include every entity defined in the
 # English locale DTDs.  (Missing an entity is a fatal error.)
 
-status=0
+status=0;
 
-cd src/chrome/locale
+cd src/chrome/locale;
 
 grep '^<!ENTITY' en/* | cut -d' ' -f2 | sort -u > ../en_entities;
 
 lang_entities_list() {
-   grep '^<!ENTITY' $lang/* | cut -d' ' -f2 | sort -u;
+   grep '^<!ENTITY' "$lang/"* | cut -d' ' -f2 | sort -u;
 }
 
 compare_lang() {
    comm -2 -3 -- ../en_entities -;
 }
 
-for lang in *
-do
-   lang_entities_list | compare_lang | grep . && echo "\_ missing for locale" $lang && status=1
-done
+for lang in *; do
+   lang_entities_list | compare_lang | grep . && echo "\_ missing for locale" "$lang" && status=1;
+done;
 
 rm ../en_entities;
 
-exit $status
+exit "$status";
