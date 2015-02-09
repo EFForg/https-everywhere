24ca919dfa15207b16afa6dd3f05a71a9f3c39d6
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2015-02-09 14:54:26 -0800

    Account for deleted files in ruleset coverage test.

--- test-ruleset-coverage.sh
+++ test-ruleset-coverage.sh
@@ -6,9 +6,11 @@
 cd $(dirname $0)
 TMP=`mktemp`
 trap 'rm "$TMP"' EXIT
+# Git log gives us all changed files. Pipe that through ls to eliminate files
+# that have been deleted.
 if ! git log --name-only --date=local --since=2015-02-05 --pretty=format: \
       src/chrome/content/rules/ | sort -u | \
-      xargs python2.7 https-everywhere-checker/src/https_everywhere_checker/check_rules.py \
+      xargs ls 2>/dev/null | xargs python2.7 https-everywhere-checker/src/https_everywhere_checker/check_rules.py \
       https-everywhere-checker/checker.config.sample ; then
   echo "Ruleset test coverage was insufficient. Please add <test url=...> tags " \
        "to ruleset with additional HTTP URLs to test rewriting and fetching."
