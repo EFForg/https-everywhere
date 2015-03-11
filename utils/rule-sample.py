dc7911561f266512103d1728f7980218c5f578a8
Peter Eckersley <pde@eff.org>
2015-03-10 17:32:42 -0700

    Detect mixed content correctly

--- utils/rule-sample.py
+++ utils/rule-sample.py
@@ -9,7 +9,7 @@ count = 0
 while count < 20:
   f = random.choice(rulefiles)
   ruleset = open(f).read()
-  if "default_off" in ruleset or "platform=mixedcontent" in ruleset:
+  if "default_off" in ruleset or 'platform="mixedcontent' in ruleset:
     print "skipping", f
     continue
   count += 1
