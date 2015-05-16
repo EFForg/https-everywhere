bafd40a68b6fb0af66e02206bd9d7ac2b72ccb08
Andrew Lorente <hello@andrewlorente.com>
2015-05-16 12:01:12 -0700

    Don't panic on failing to find a utility [#1504]
    Whoops! `which` will exit non-0 if it doesn't find its argument, killing
    the script when running with -e.

--- locate-gnu-utils.sh
+++ locate-gnu-utils.sh
@@ -4,22 +4,22 @@
 # OSX systems we'll have g-prefixed GNU versions installed by Homebrew. Real
 # BSD systems are still left in the cold.
 
-READLINK=$(which readlink 2>/dev/null)
-GREADLINK=$(which greadlink 2>/dev/null)
+READLINK=$(which readlink 2>/dev/null || true)
+GREADLINK=$(which greadlink 2>/dev/null || true)
 if [ -x "$GREADLINK" ]; then
   READLINK="$GREADLINK"
 fi
 unset GREADLINK
 
-MKTEMP=$(which mktemp 2>/dev/null)
-GMKTEMP=$(which gmktemp 2>/dev/null)
+MKTEMP=$(which mktemp 2>/dev/null || true)
+GMKTEMP=$(which gmktemp 2>/dev/null || true)
 if [ -x "$GMKTEMP" ]; then
   MKTEMP="$GMKTEMP"
 fi
 unset GMKTEMP
 
-SHA256SUM=$(which sha256sum 2>/dev/null)
-GSHA256SUM=$(which gsha256sum 2>/dev/null)
+SHA256SUM=$(which sha256sum 2>/dev/null || true)
+GSHA256SUM=$(which gsha256sum 2>/dev/null || true)
 if [ -x "$GSHA256SUM" ]; then
   SHA256SUM="$GSHA256SUM"
 fi
