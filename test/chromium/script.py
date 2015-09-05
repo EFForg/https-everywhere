a66b64c1d885707abc2c45513e7cecae71003fd4
Nick Semenkovich <semenko@alum.mit.edu>
2015-09-05 18:52:05 -0500

    Close driver on failure too, misspelling fix.
    Signed-off-by: Nick Semenkovich <semenko@alum.mit.edu>

--- test/chromium/script.py
+++ test/chromium/script.py
@@ -28,16 +28,16 @@ chromeOps = webdriver.ChromeOptions()
 chromeOps.add_extension(sys.argv[1])
 chromeOps.add_argument("--disable-setuid-sandbox")
 
-chromdriver_path = "chromedriver"
+chromedriver_path = "chromedriver"
 
 if 'TRAVIS' in os.environ:
     chromeOps.add_argument('--no-sandbox')
-    chromdriver_path = os.path.abspath("test/chromium/chromedriver")
+    chromedriver_path = os.path.abspath("test/chromium/chromedriver")
 
 # First argument is optional, if not specified will search path.
 
 try:
-    driver = webdriver.Chrome(chromdriver_path, chrome_options=chromeOps)
+    driver = webdriver.Chrome(chromedriver_path, chrome_options=chromeOps)
 except WebDriverException as e:
     error = e.__str__()
 
@@ -55,12 +55,18 @@ driver.get('http://libssh.org/robots.txt')
 
 #Page Loaded
 
+failed = False
 if driver.current_url.startswith('https'):
     print bcolors.OKGREEN + "HTTP to HTTPS redirection successful" + bcolors.ENDC
 elif driver.current_url.startswith('http'):
     print bcolors.FAIL + "HTTP to HTTPS redirection failed" + bcolors.ENDC
+    failed = True
     sys.exit(1)
 
 print '' #New line
 
 driver.quit()
+
+
+if failed:
+    sys.exit(1)
