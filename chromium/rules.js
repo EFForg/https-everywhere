17408bcd72d3957a71119695188416d0f8f89212
Ingvar Stepanyan <me@rreverser.com>
2015-09-22 17:32:29 +0100

    Remove redundant `.value`

--- chromium/rules.js
+++ chromium/rules.js
@@ -188,7 +188,7 @@ RuleSets.prototype = {
     var default_off = ruletag.getAttribute("default_off");
     if (default_off) {
       default_state = false;
-      note += default_off.value + "\n";
+      note += default_off + "\n";
     }
 
     // If a ruleset declares a platform, and we don't match it, treat it as
