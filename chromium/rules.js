7d5bb037504beed1e1827527955d87d70ae21658
Ingvar Stepanyan <me@rreverser.com>
2016-03-08 13:56:26 +0000

    Stub all log types to execute other methods in Node.js

--- chromium/rules.js
+++ chromium/rules.js
@@ -1,6 +1,10 @@
 "use strict";
 // Stubs so this runs under nodejs. They get overwritten later by util.js
-var DBUG = 1;
+var VERB=1;
+var DBUG=2;
+var INFO=3;
+var NOTE=4;
+var WARN=5;
 function log(){}
 
 // To reduce memory usage for the numerous rules/cookies with trivial rules
