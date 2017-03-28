689f799a3ec91697df075b3562bef6802281f99b
Bisaloo <Bisaloo@users.noreply.github.com>
2017-03-28 17:29:48 +0200

    Create duplicate-whitelist-cleanup.sh

---
+++ utils/duplicate-whitelist-cleanup.sh
@@ -0,0 +1,6 @@
+cat utils/duplicate-whitelist.txt | while read -r line; do
+    REPEATS=`grep "target host=\"$line\"" src/chrome/content/rules/*.xml | wc -l`
+    if [ "$REPEATS" -gt "1" ]; then
+        echo "$line"
+    fi
+done >| utils/duplicate-whitelist.txt
