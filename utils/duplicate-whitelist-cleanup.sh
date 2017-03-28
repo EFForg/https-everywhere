d28fd28580f585cf9be032b00f390938ed32c97d
Bisaloo <Bisaloo@users.noreply.github.com>
2017-03-28 17:31:29 +0200

    Add shebang

--- utils/duplicate-whitelist-cleanup.sh
+++ utils/duplicate-whitelist-cleanup.sh
@@ -1,3 +1,5 @@
+#! /bin/sh
+
 cat utils/duplicate-whitelist.txt | while read -r line; do
     REPEATS=`grep "target host=\"$line\"" src/chrome/content/rules/*.xml | wc -l`
     if [ "$REPEATS" -gt "1" ]; then
