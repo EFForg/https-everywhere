6f8495e4ba430101e54351a91ab1f4fb17cfa149
Bisaloo <Bisaloo@users.noreply.github.com>
2017-03-28 17:58:19 +0200

    Don't use regex in grep

--- utils/duplicate-whitelist-cleanup.sh
+++ utils/duplicate-whitelist-cleanup.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 cat utils/duplicate-whitelist.txt | while read -r line; do
-    REPEATS=`grep "target host=\"$line\"" src/chrome/content/rules/*.xml | wc -l`
+    REPEATS=`grep -F "target host=\"$line\"" src/chrome/content/rules/*.xml | wc -l`
     if [ "$REPEATS" -gt "1" ]; then
         echo "$line"
     fi
