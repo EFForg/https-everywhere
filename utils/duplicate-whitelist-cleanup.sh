da8ff2cdbb6f425016a8edbaa435b435c55a6bcd
Bisaloo <Bisaloo@users.noreply.github.com>
2017-04-14 09:05:54 +0200

    Remove unnecessary cat

--- utils/duplicate-whitelist-cleanup.sh
+++ utils/duplicate-whitelist-cleanup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 for host in `cat utils/duplicate-whitelist.txt`; do
-    REPEATS=`grep -F "target host=\"$host\"" src/chrome/content/rules/*.xml | cat | wc -l`
+    REPEATS=`grep -F "target host=\"$host\"" src/chrome/content/rules/*.xml | wc -l`
     if [ $REPEATS -gt 1 ]; then
         echo $host
     fi
