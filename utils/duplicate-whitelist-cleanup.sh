d8048d5bbea064b87a0a302410810703894ab00b
Bisaloo <Bisaloo@users.noreply.github.com>
2017-04-14 08:26:05 +0200

    Simpler loop

--- utils/duplicate-whitelist-cleanup.sh
+++ utils/duplicate-whitelist-cleanup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cat utils/duplicate-whitelist.txt | while read -r host; do
+for host in `cat utils/duplicate-whitelist.txt`; do
     REPEATS=`grep -F "target host=\"$host\"" src/chrome/content/rules/*.xml | cat | wc -l`
     if [ "$REPEATS" -gt "1" ]; then
         echo "$host"
