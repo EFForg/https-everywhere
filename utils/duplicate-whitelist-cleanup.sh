d132cb709ea0c5019f222e5485b79372357ea26f
Bisaloo <Bisaloo@users.noreply.github.com>
2017-04-14 08:59:42 +0200

    Remove unnecessary quotes

--- utils/duplicate-whitelist-cleanup.sh
+++ utils/duplicate-whitelist-cleanup.sh
@@ -2,7 +2,7 @@
 
 for host in `cat utils/duplicate-whitelist.txt`; do
     REPEATS=`grep -F "target host=\"$host\"" src/chrome/content/rules/*.xml | cat | wc -l`
-    if [ "$REPEATS" -gt "1" ]; then
-        echo "$host"
+    if [ $REPEATS -gt 1 ]; then
+        echo $host
     fi
 done >| utils/duplicate-whitelist.txt
