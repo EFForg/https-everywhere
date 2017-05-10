7d45aa4117fcdfd60714c116ad0522a2c3c9dfed
William Budington <bill@eff.org>
2017-05-10 14:40:01 -0700

    Remove XPCOM version changes from utils/setversion.py

--- utils/setversion.py
+++ utils/setversion.py
@@ -2,7 +2,6 @@
 from datetime import date
 import json
 import re
-import sys
 
 # Set the version in manifest.json to one based on today's date.
 # Set the version in install.rdf and about.xul to one specified on the command
@@ -15,17 +14,3 @@ f.close()
 manifest['version'] = `t.year` +'.'+ `t.month` +'.'+ `t.day`
 f = open('chromium/manifest.json','w')
 f.write(json.dumps(manifest,indent=4,sort_keys=True,separators=(',', ': ')))
-
-def replace_in_file(from_re, to, filename):
-    contents = open(filename).read()
-    with open(filename, 'w') as outfile:
-        outfile.write(re.sub(from_re, to, contents))
-
-firefox_version = sys.argv[1]
-replace_in_file('<em:version>.*</em:version>',
-          '<em:version>' + firefox_version + '</em:version>',
-          'src/install.rdf')
-
-replace_in_file('(?s)(https-everywhere.about.version.*?<label>).*?</label>',
-          '\g<1>' + firefox_version + '</label>',
-          'src/chrome/content/about.xul')
