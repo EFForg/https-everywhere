cee5ed256115c3c7f3c981da6e01d831345ae62d
AJ Jordan <alex@strugee.net>
2017-06-19 02:36:10 -0700

    Import Probot plugin template

---
+++ utils/issue-format-bot/README.md
@@ -0,0 +1,15 @@
+# issue-format-bot
+
+> a GitHub Integration built with [probot](https://github.com/probot/probot) that parses incoming HTTPS Everywhere ruleset bugs
+
+## Setup
+
+```
+# Install dependencies
+npm install
+
+# Run the bot
+npm start
+```
+
+See [docs/deploy.md](docs/deploy.md) if you would like to run your own instance of this plugin.
---
+++ utils/issue-format-bot/index.js
@@ -0,0 +1,10 @@
+module.exports = (robot) => {
+  // Your plugin code here
+  console.log('Yay, the plugin was loaded!');
+  
+  // For more information on building plugins:
+  // https://github.com/probot/probot/blob/master/docs/plugins.md
+  
+  // To get your plugin running against GitHub, see:
+  // https://github.com/probot/probot/blob/master/docs/development.md
+};
