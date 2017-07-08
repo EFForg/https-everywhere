b187608dcc27b77da4ae52d9d6577be1edc986cb
koops76 <koops76@users.noreply.github.com>
2017-07-08 10:55:39 +0300

    Update rules.js

--- chromium/rules.js
+++ chromium/rules.js
@@ -452,7 +452,7 @@
       }
 
       window.log(window.INFO, 'Testing securecookie applicability with ' + testUri);
-      const potentiallyApplicable = this.potentiallyApplicableRulesets(domain);
+      const potentiallyApplicable = this.potentiallyApplicableRulesets(domain).values();
       const secure = potentiallyApplicable.some((ruleset) => ruleset.active && ruleset.apply(testUri));
 
       if (secure) {
