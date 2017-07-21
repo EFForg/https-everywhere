50dc6e30e69c7463269e4887d0699c7bae2d15aa
koops76 <koops76@users.noreply.github.com>
2017-07-21 16:49:02 -0700

    Use URL object instead of document.createElement('a')

--- chromium/background.js
+++ chromium/background.js
@@ -227,8 +227,7 @@ function onBeforeRequest(details) {
     return;
   }
 
-  var uri = document.createElement('a');
-  uri.href = details.url;
+  const uri = new URL(details.url);
 
   // Should the request be canceled?
   var shouldCancel = (
@@ -294,8 +293,7 @@ function onBeforeRequest(details) {
 
   if (newuristr && using_credentials_in_url) {
     // re-insert userpass info which was stripped temporarily
-    var uri_with_credentials = document.createElement('a');
-    uri_with_credentials.href = newuristr;
+    const uri_with_credentials = new URL(newuristr);
     uri_with_credentials.username = tmp_user;
     uri_with_credentials.password = tmp_pass;
     newuristr = uri_with_credentials.href;
