e5673d74fb98daa9d5bf6c52acd283ad5b98c896
AJ Jordan <alex@strugee.net>
2017-08-01 17:28:13 -0400

    Support issue edits

--- utils/issue-format-bot/index.js
+++ utils/issue-format-bot/index.js
@@ -1,10 +1,12 @@
 'use strict';
 
 const newissue = require('./lib/newissue');
+const issueedit = require('./lib/issueedit');
 
 module.exports = robot => {
 	robot.log('HTTPS Everywhere ruleset parser started.');
 
 	// TODO parse issue edits too
 	robot.on('issues.opened', newissue(robot));
+	robot.on('issues.edited', issueedit(robot));
 };
---
+++ utils/issue-format-bot/test/issueedit-handler-test.js
@@ -0,0 +1,112 @@
+'use strict';
+
+const vows = require('perjury'),
+      assert = vows.assert,
+      robot = require('./mocks/robot'),
+      makeContext = require('./mocks/context');
+
+// TODO make this share code with newissue-handler-test
+
+vows.describe('issue edit handler').addBatch({
+	'When we require the module': {
+		topic: function() {
+			return require('../lib/issueedit');
+		},
+		'it works': function(err, newissue) {
+			assert.ifError(err);
+		},
+		'it\'s a function': function(err, newissue) {
+			assert.isFunction(newissue);
+		},
+		'and we initialize it with a robot': {
+			topic: function(newissue) {
+				return newissue(robot);
+			},
+			'it works': function(err, handler) {
+				assert.ifError(err);
+			},
+			'it returns a function': function(err, handler) {
+				assert.isFunction(handler);
+			},
+			'and we pass it the context of an issue edit with a null body': {
+				topic: function(handler) {
+					const context = makeContext.issue('');
+
+					handler(context);
+
+					return context;
+				},
+				'it works': function(err) {
+					assert.ifError(err);
+				},
+				'it only creates one comment': function(err, context) {
+					assert.isTrue(context.issue.calledOnce);
+				},
+				'it creates a comment with the right message': function(err, context) {
+					// args[0] is first call arguments, second [0] is first arg
+					assert.isObject(context.issue.args[0][0]);
+					// TODO try to find a more decoupled way than matching text
+					assert.isTrue(context.issue.args[0][0].body.includes('don\'t see any text'));
+				}
+			},
+			'and we pass it the context of a issue edit with a bad type': {
+				topic: function(handler) {
+					const context = makeContext.issue('Type: invalid');
+
+					handler(context);
+
+					return context;
+				},
+				'it works': function(err) {
+					assert.ifError(err);
+				},
+				'it only creates one comment': function(err, context) {
+					assert.isTrue(context.issue.calledOnce);
+				},
+				'it creates a comment with the right message': function(err, context) {
+					// args[0] is first call arguments, second [0] is first arg
+					assert.isObject(context.issue.args[0][0]);
+					// TODO try to find a more decoupled way than matching text
+					assert.isTrue(context.issue.args[0][0].body.includes('type of issue'));
+				}
+			},
+			'and we pass it the context of a issue edit with a type of "new ruleset" and a correct body': {
+				topic: function(handler) {
+					const context = makeContext.issue('Type: new ruleset\nDomain: example.com');
+
+					handler(context);
+
+					return context;
+				},
+				'it works': function(err) {
+					assert.ifError(err);
+				},
+				'it doesn\'t comment': function(err, context) {
+					assert.isTrue(context.issue.notCalled);
+				}
+				// TODO useful tests here
+			},
+			'and we pass it the context of a issue edit with a type of "new ruleset" and a problematic body': {
+				topic: function(handler) {
+					const context = makeContext.issue('Type: new ruleset');
+
+					handler(context);
+
+					return context;
+				},
+				'it works': function(err) {
+					assert.ifError(err);
+				},
+				'it only creates one comment': function(err, context) {
+					assert.isTrue(context.issue.calledOnce);
+				},
+				'it includes the problem': function(err, context) {
+					// args[0] is first call arguments, second [0] is first arg
+					assert.isObject(context.issue.args[0][0]);
+					// TODO try to find a more decoupled way than matching text
+					assert.isTrue(context.issue.args[0][0].body.includes('missing domain information'));
+				}
+			}
+		}
+	}
+}).export(module);
