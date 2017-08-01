78ab51484e0bf8f4f9a40e4d71a3ae90f832126a
AJ Jordan <alex@strugee.net>
2017-08-01 03:38:22 -0400

    Don't make the context issue factory a stub
    I was just being dumb. Obviously we want the functions _inside_ the
    returned context to be Sinon stubs, not the factory function itself.

--- utils/issue-format-bot/test/mocks/context.js
+++ utils/issue-format-bot/test/mocks/context.js
@@ -5,18 +5,19 @@ const sinon = require('sinon');
 let issueNumber = 1;
 
 module.exports = {
-	issue: sinon.stub().returns({
-		payload: {
-			issue: {
-				// TODO this doesn't actually work because of static .returns({})
-				number: issueNumber++
-			}
-		},
-		github: {
-			issues: {
-				createComment: sinon.spy()
-			}
-		},
-		issue: sinon.spy()
-	})
+	issue() {
+		return {
+			payload: {
+				issue: {
+					number: issueNumber++
+				}
+			},
+			github: {
+				issues: {
+					createComment: sinon.spy()
+				}
+			},
+			issue: sinon.spy()
+		};
+	}
 };
