9e3194adf2896af2a9ccd59e295e3b6336ec97cb
AJ Jordan <alex@strugee.net>
2017-08-02 21:03:29 -0400

    Remove outdated TODO

--- utils/issue-format-bot/test/newissue-handler-test.js
+++ utils/issue-format-bot/test/newissue-handler-test.js
@@ -10,7 +10,6 @@ vows.describe('new issue handler').addBatch(
 		'and we pass it the context of a new issue with a null body': handlerutil.nullBody('can\'t find any text'),
 		'and we pass it the context of a new issue with a bad type': handlerutil.badType('type of issue'),
 		'and we pass it the context of a new issue with a type of "new ruleset" and a correct body': handlerutil.correctNewRuleset,
-		// Problematic bodies aren't tested extensively here because that validation will eventually be refactored into another module and tested there
 		'and we pass it the context of a new issue with a type of "new ruleset" and a problematic body': handlerutil.problematicNewRuleset('missing domain information')
 	})
 ).export(module);
