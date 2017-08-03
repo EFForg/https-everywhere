c75c28335f119f29d1081b820bc2acea3cfc27d3
AJ Jordan <alex@strugee.net>
2017-08-02 23:59:54 -0400

    Add AGPL comment headers

--- utils/issue-format-bot/test/mocks/context.js
+++ utils/issue-format-bot/test/mocks/context.js
@@ -1,3 +1,20 @@
+// This file is part of the HTTPS Everywhere issue management bot.
+//
+// The HTTPS Everywhere issue management bot is free software: you can
+// redistribute it and/or modify it under the terms of the GNU Affero
+// General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option)
+// any later version.
+//
+// The HTTPS Everywhere issue management bot is distributed in the
+// hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+// the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+// PURPOSE.  See the GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public
+// License along with the HTTPS Everywhere issue management bot.  If
+// not, see <http://www.gnu.org/licenses/>.
+
 'use strict';
 
 const sinon = require('sinon');
