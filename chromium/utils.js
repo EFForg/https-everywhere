205258f3021d36f9e743349e20d61dd91c439453
koops76 <koops76@users.noreply.github.com>
2017-08-07 19:31:08 +0300

    oops

--- chromium/utils.js
+++ chromium/utils.js
@@ -2,9 +2,11 @@
 
 // Logging everything is *very* slow since the browser will retain console logs even when the developer tools window is closed.
 
-console.log('Hey developer! Want to see more verbose logging?');
-console.log('Type this into the console: utils.minLogLevel = 1');
-console.log('Accepted levels are 1-5, default is 4.');
+if (typeof window !== 'undefined') {
+  console.log('Hey developer! Want to see more verbose logging?');
+  console.log('Type this into the console: utils.minLogLevel = 1');
+  console.log('Accepted levels are 1-5, default is 4.');
+}
 
 const utils = {
   minLogLevel: 4,
