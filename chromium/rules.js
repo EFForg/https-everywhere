15de3c99b0e7cb2ec8fe669855dc338b0a0aaeac
Ingvar Stepanyan <me@rreverser.com>
2017-09-14 16:44:56 +0100

    Reuse trivial Rule, not just properties
    Before:
    Initial usage: 42.1 MB
    Maximum usage: 80.2 MB
    
    After:
    Initial usage: 41 MB
    Maximum usage: 79.2 MB

--- chromium/rules.js
+++ chromium/rules.js
@@ -21,26 +21,33 @@ let settings = {
 };
 
 // To reduce memory usage for the numerous rules/cookies with trivial rules
-const trivial_rule_to = "https:";
-const trivial_rule_from_c = new RegExp("^http:");
 const trivial_cookie_name_c = new RegExp(".*");
 const trivial_cookie_host_c = new RegExp(".*");
 
 /**
- * A single rule
+ * Constructs a single rule
  * @param from
  * @param to
  * @constructor
  */
 function Rule(from, to) {
+  this.from_c = new RegExp(from);
+  this.to = to;
+}
+
+// To reduce memory usage for the numerous rules/cookies with trivial rules
+const trivial_rule = new Rule("^http:", "https:");
+
+/**
+ * Returns a common trivial rule or constructs a new one.
+ */
+function getRule(from, to) {
   if (from === "^http:" && to === "https:") {
     // This is a trivial rule, rewriting http->https with no complex RegExp.
-    this.to = trivial_rule_to;
-    this.from_c = trivial_rule_from_c;
+    return trivial_rule;
   } else {
     // This is a non-trivial rule.
-    this.to = to;
-    this.from_c = new RegExp(from);
+    return new Rule(from, to);
   }
 }
 
@@ -256,7 +263,7 @@ RuleSets.prototype = {
     var rules = ruletag["rule"];
     for (let rule of rules) {
       if (rule["from"] != null && rule["to"] != null) {
-        rule_set.rules.push(new Rule(rule["from"], rule["to"]));
+        rule_set.rules.push(getRule(rule["from"], rule["to"]));
       }
     }
 
@@ -303,7 +310,7 @@ RuleSets.prototype = {
   addUserRule : function(params) {
     util.log(util.INFO, 'adding new user rule for ' + JSON.stringify(params));
     var new_rule_set = new RuleSet(params.host, true, "user rule");
-    var new_rule = new Rule(params.urlMatcher, params.redirectTo);
+    var new_rule = getRule(params.urlMatcher, params.redirectTo);
     new_rule_set.rules.push(new_rule);
     if (!this.targets.has(params.host)) {
       this.targets.set(params.host, []);
@@ -376,7 +383,7 @@ RuleSets.prototype = {
 
     var rules = ruletag.getElementsByTagName("rule");
     for (let rule of rules) {
-      rule_set.rules.push(new Rule(rule.getAttribute("from"),
+      rule_set.rules.push(getRule(rule.getAttribute("from"),
         rule.getAttribute("to")));
     }
 
