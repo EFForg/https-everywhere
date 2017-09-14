65e996979df0ad213507bea878d9f85cfaaec791
koops76 <koops76@users.noreply.github.com>
2017-09-14 03:36:29 -0700

    Let's use the same badge color as uBlock Origin.

--- chromium/background.js
+++ chromium/background.js
@@ -196,7 +196,7 @@ function updateState () {
 
     const activeCount = getActiveRulesetCount(tabs[0].id);
 
-    chrome.browserAction.setBadgeBackgroundColor({ color: '#00cc00' });
+    chrome.browserAction.setBadgeBackgroundColor({ color: '#666666' });
 
     const showBadge = activeCount > 0 && isExtensionEnabled && showCounter;
 
