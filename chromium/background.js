0b54185372631954ed8620f7355b4a8b3e512ab4
koops76 <koops76@users.noreply.github.com>
2017-09-19 05:41:13 +0300

    Update background.js

--- chromium/background.js
+++ chromium/background.js
@@ -413,7 +413,7 @@
     if (switchPlannerEnabledFor[details.tabId] && uri.protocol !== "https:") {
       writeToSwitchPlanner(details.type,
         details.tabId,
-        canonical_host,
+        uri.hostname,
         details.url,
         newuristr);
     }
