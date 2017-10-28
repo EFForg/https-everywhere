6387c2090c2f30587877b8d629d30536f03316d2
William Budington <bill@eff.org>
2017-10-27 19:17:46 -0700

    Remove https-everywhere-updates.json

--- utils/setversion.py
+++ utils/setversion.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python2.7
 from datetime import date
 import json
-import re
 
 
 # Set the version in manifest.json to one based on today's date.
@@ -15,23 +14,3 @@ f.close()
 manifest['version'] = version
 f = open('chromium/manifest.json','w')
 f.write(json.dumps(manifest,indent=4,sort_keys=True,separators=(',', ': ')))
-
-
-# Add a link to the current version in https-everywhere-updates.json
-
-updates_hash = {
-    "version": version,
-    "update_link": "https://eff.org/files/https-everywhere-" + version + "-eff.xpi",
-    "applications": {
-        "gecko": {
-            "strict_min_version": "52.0"
-        }
-    }
-}
-
-f = open('src/https-everywhere-updates.json')
-updates = json.loads(f.read())
-f.close()
-updates['addons']['https-everywhere-eff@eff.org']['updates'].append(updates_hash);
-f = open('src/https-everywhere-updates.json', 'w')
-f.write(json.dumps(updates,indent=4,sort_keys=True,separators=(',', ': ')))
