45b57be2936f5cd089fc9cde890663efdece3d33
koops76 <koops76@users.noreply.github.com>
2017-11-07 19:10:12 +0300

    Update background.js

--- chromium/background.js
+++ chromium/background.js
@@ -230,10 +230,10 @@ function onBeforeRequest(details) {
     !uri.protocol === 'https:' &&
     !uri.hostname.slice(-6) === '.onion' &&
     !uri.hostname === 'localhost' &&
-    !uri.hostname === '[::1]' &&
     !/^127(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9]{1,2})){3}$/.test(uri.hostname) &&
-    !uri.hostname === '[::]' &&
-    !uri.hostname === '0.0.0.0';
+    !uri.hostname === '0.0.0.0' &&
+    !uri.hostname === '[::1]' &&
+    !uri.hostname === '[::]';
 
   // Normalise hosts such as "www.example.com."
   var canonical_host = uri.hostname;
