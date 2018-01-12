028c9c3c73da3c47dc25304b59beb17a99d53268
William Budington <bill@eff.org>
2018-01-12 15:08:56 -0800

    Use store module for storage

--- chromium/background-scripts/update.js
+++ chromium/background-scripts/update.js
@@ -2,6 +2,8 @@
 
 (function(exports) {
 
+const store = require('./store');
+
 // how often we should check for new rulesets
 const periodicity = 10;
 
@@ -21,29 +23,9 @@ for(let update_channel of update_channels){
 }
 
 
-// Get an object stored in localstorage
-function getStoredLocalObject(object_key){
-  return new Promise(resolve => {
-    chrome.storage.local.get(object_key, root => {
-      resolve(root[object_key]);
-    });
-  });
-}
-
-// Set an object stored in localstorage
-function setStoredLocalObject(object_key, object_value){
-  return new Promise(resolve => {
-    var object = {};
-    object[object_key] = object_value;
-    chrome.storage.local.set(object, () => {
-      resolve();
-    });
-  });
-}
-
 // Determine the time until we should check for new rulesets
 async function timeToNextCheck() {
-  const last_checked = await getStoredLocalObject('last-checked');
+  const last_checked = await store.get_promise('last-checked');
   if(last_checked === undefined) {
     return 0;
   } else {
@@ -74,7 +56,7 @@ async function checkForNewRulesets(update_channel) {
   let timestamp_promise = xhr_promise(update_channel.update_path_prefix + "/rulesets-timestamp");
   let rulesets_timestamp = Number(await timestamp_promise);
 
-  if((await getStoredLocalObject('rulesets-timestamp: ' + update_channel.name) || 0) < rulesets_timestamp){
+  if((await store.get_promise('rulesets-timestamp: ' + update_channel.name) || 0) < rulesets_timestamp){
     return rulesets_timestamp;
   } else {
     return false;
@@ -84,7 +66,7 @@ async function checkForNewRulesets(update_channel) {
 // Download and return new rulesets
 async function getNewRulesets(rulesets_timestamp, update_channel) {
 
-  setStoredLocalObject('rulesets-timestamp: ' + update_channel.name, rulesets_timestamp);
+  store.set_promise('rulesets-timestamp: ' + update_channel.name, rulesets_timestamp);
 
   let signature_promise = xhr_promise(update_channel.update_path_prefix + "/rulesets-signature.sha256.base64");
   let rulesets_promise = xhr_promise(update_channel.update_path_prefix + "/default.rulesets.gz.base64");
@@ -118,7 +100,7 @@ function verifyAndStoreNewRulesets(new_rulesets, update_channel){
       .then(async isvalid => {
         if(isvalid) {
           util.log(util.NOTE, update_channel.name + ': Downloaded ruleset signature checks out.  Storing rulesets.');
-          await setStoredLocalObject('rulesets: ' + update_channel.name, new_rulesets.rulesets_gz_base64);
+          await store.set_promise('rulesets: ' + update_channel.name, new_rulesets.rulesets_gz_base64);
           resolve(true);
         } else {
           reject('Downloaded ruleset signature is invalid.  Aborting.');
@@ -140,7 +122,7 @@ async function applyStoredRulesets(){
   for(let update_channel of update_channels){
     rulesets_promises.push(new Promise(resolve => {
       const key = 'rulesets: ' + update_channel.name
-      chrome.storage.local.get(key, root => {
+      store.get(key, root => {
         if(root[key]){
           util.log(util.NOTE, update_channel.name + ': Applying stored rulesets.');
 
@@ -167,32 +149,12 @@ async function applyStoredRulesets(){
   }
 }
 
-/*
-async function storeUpdateObjects() {
-  let local_update_channels = await getStoredLocalObject('update-channels');
-  if(local_update_channels == undefined){
-    let update_promises = [];
-    let update_channels_array = [];
-    for(let update_channel of update_channels){
-      update_promises.push(setStoredLocalObject(
-        'update-channel-' + update_channel.name,
-        update_channels
-      ));
-
-      update_channels_array.push(update_channel.name);
-    }
-
-    update_promises.push(setStoredLocalObject('update-channels', update_channels_array));
-    await Promise.all(update_promises);
-  }
-}*/
-
 // basic workflow for periodic checks
 async function performCheck() {
   util.log(util.NOTE, 'Checking for new rulesets.');
 
   const current_timestamp = Date.now() / 1000;
-  setStoredLocalObject('last-checked', current_timestamp);
+  store.set_promise('last-checked', current_timestamp);
 
   let num_updates = 0;
   for(let update_channel of update_channels){
