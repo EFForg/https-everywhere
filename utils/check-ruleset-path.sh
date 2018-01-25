40fa6ecde3f38ec13910adada8252fb5d9492a4c
Pasu Chan Chak Shing <ccspasu@gmail.com>
2018-01-25 19:09:29 +0800

    Update utils/check-ruleset-path.sh

--- utils/check-ruleset-path.sh
+++ utils/check-ruleset-path.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CHANGED_FILES=`git diff --name-only $COMMON_BASE_COMMIT | grep -v $RULESET_FOLDER | grep '.xml'`
+CHANGED_FILES=`git diff --name-only $COMMON_BASE_COMMIT | grep -v $RULESETFOLDER | grep '.xml'`
 
 EXIT_CODE=0
 
@@ -11,7 +11,7 @@ if [ "$CHANGED_FILES" != "" ]; then
 
     # if file matched the RegExp
     if [ $? -eq 0 ]; then
-      echo >&2 "ERROR: $FILE Inclusion of ruleset outside of $RULESET_FOLDER"
+      echo >&2 "ERROR: $FILE Inclusion of ruleset outside of $RULESETFOLDER"
       EXIT_CODE=1
     fi
   done <<< "$CHANGED_FILES"
