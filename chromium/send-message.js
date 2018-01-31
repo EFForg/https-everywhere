f62cda124d74a2c3dc785c7f939c2f157c536cfa
William Budington <bill@eff.org>
2018-01-30 18:24:42 -0800

    Pass both type and object directly through (#14487)

--- chromium/send-message.js
+++ chromium/send-message.js
@@ -3,10 +3,5 @@
 "use strict";
 
 function sendMessage(type, object, callback) {
-  var packet = {};
-  packet.type = type;
-  if(object){
-    packet.object = object;
-  }
-  chrome.runtime.sendMessage(packet, callback);
+  chrome.runtime.sendMessage({ type, object }, callback);
 }
