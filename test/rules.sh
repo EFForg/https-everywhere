950f5d620bc10d4e3e533d4b8527fb93f94e2615
William Budington <bill@eff.org>
2018-02-08 14:18:00 -0800

    Fix rules

--- test/rules.sh
+++ test/rules.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 python2.7 utils/ruleset_filenames_validate.py
-RULESETFOLDER="$RULESETFOLDER" COMMON_BASE_COMMIT="$COMMON_BASE_COMMIT" utils/check-ruleset-path.sh
+utils/check-ruleset-path.sh
 utils/remove-obsolete-references.sh
 python2.7 utils/validate.py
 python2.7 utils/trivial-validate.py --quiet
