1a82ea014fe3687360419480c16823ac9748643f
William Budington <bill@eff.org>
2018-02-08 20:21:32 -0800

    Convert relaxng validation to py3.6

--- test/validations.sh
+++ test/validations.sh
@@ -6,5 +6,5 @@ test/validations/path/run.sh
 test/validations/test-coverage/run.sh
 python3.6 test/validations/securecookie/run.py
 python3.6 test/validations/filename/run.py
-python2.7 test/validations/relaxng/run.py
+python3.6 test/validations/relaxng/run.py
 python2.7 test/validations/special/run.py --quiet
