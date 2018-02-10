effb5680440a423e7510dba7307d695f25fad386
Hugo Gruson <hugo.gruson@openmailbox.org>
2018-02-10 13:46:10 +0100

    Use better regex in cleanup script

--- test/validations/special/duplicate-whitelist-cleanup.sh
+++ test/validations/special/duplicate-whitelist-cleanup.sh
@@ -4,7 +4,7 @@ TMPFILE=`mktemp /tmp/buffer.XXXXXXXX`
 trap 'rm "$TMPFILE"' EXIT
 
 for host in `cat test/validations/special/duplicate-whitelist.txt`; do
-    REPEATS=`grep -F "target host=\"$host\"" src/chrome/content/rules/*.xml | wc -l`
+    REPEATS=`egrep -l "<target\s+host=\s*\"$host\"\s*/>" src/chrome/content/rules/*.xml | wc -l`
     if [ $REPEATS -gt 1 ]; then
         echo $host
     fi
