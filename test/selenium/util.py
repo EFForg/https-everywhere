a8844b6c8d51f067202e8c039944c8d0bba21d08
William Budington <bill@eff.org>
2018-02-22 15:03:16 -0800

    fixup

--- test/selenium/util.py
+++ test/selenium/util.py
@@ -77,14 +77,14 @@ class ExtensionTestCase(unittest.TestCase):
     @contextmanager
     def load_popup_for(self, url='about:blank'):
         create_url_and_popup_js_str = '''
-        (function(done) {
-          chrome.tabs.create({url: '{}'}, function(tab) {
+        (function(done) {{
+          chrome.tabs.create({{url: '{}'}, function(tab) {{
             setTimeout(
-              () => chrome.tabs.create({url: '{}' + '?tabId=' + String(tab.id)}, done),
+              () => chrome.tabs.create({{url: '{}' + '?tabId=' + String(tab.id)}}, done),
               500
             );
-          });
-        })(arguments[0]);
+          }});
+        }})(arguments[0]);
         '''
         script = create_url_and_popup_js_str.format(url, self.shim.popup_url)
 
