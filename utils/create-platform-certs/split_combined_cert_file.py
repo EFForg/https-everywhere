5d2fd26b08f74b986c46dcb16534e2bbcf9f7485
William Budington <bill@eff.org>
2018-02-28 15:48:02 -0800

    Update split_combined_cert_file.py arguments and create output_dir if it does not exist

--- utils/create-platform-certs/split_combined_cert_file.py
+++ utils/create-platform-certs/split_combined_cert_file.py
@@ -1,13 +1,14 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3.6
 import argparse
 import codecs
 import os
+import pathlib
 import re
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        'input_filename',
+        'cert_file',
         help='combined crt/pem file to split',
     )
     parser.add_argument(
@@ -15,18 +16,19 @@ if __name__ == '__main__':
         help='output directory for split files',
     )
     args = parser.parse_args()
-    with codecs.open(args.input_filename, 'r', encoding='utf-8') as input_file:
-        bundle = input_file.read()
+    with codecs.open(args.cert_file, 'r', encoding='utf-8') as cert_file:
+        bundle = cert_file.read()
         certs = re.compile('\n{2,}').split(bundle)
 
         # First element of certs is a comment
         certs = certs[1:]
         cert_filename_num_width = len(str(len(certs)))
+        pathlib.Path(args.output_dir).mkdir(parents=True, exist_ok=True)
         for index, cert in enumerate(certs):
             cert_name, cert_content = re.compile("\n=+\n").split(cert)
             cert_filename = os.path.join(
                 args.output_dir,
-                "cert%s.pem" % str(index+1).zfill(cert_filename_num_width),
+                "cert{}.pem".format(str(index+1).zfill(cert_filename_num_width))
             )
             with codecs.open(
                     cert_filename, 'w', encoding='utf-8') as cert_file:
