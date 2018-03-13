0972dbc1cd316e6d82ac24503ac55f65b3515f7a
William Budington <bill@eff.org>
2018-03-13 16:42:31 -0700

    Fixing duplicate-whitelist-cleanup.sh to regex escape host

--- test/validations/special/duplicate-whitelist-cleanup.sh
+++ test/validations/special/duplicate-whitelist-cleanup.sh
@@ -4,7 +4,8 @@ TMPFILE=`mktemp /tmp/buffer.XXXXXXXX`
 trap 'rm "$TMPFILE"' EXIT
 
 for host in `cat test/validations/special/duplicate-whitelist.txt`; do
-    REPEATS=`egrep -l "<target\s+host=\s*\"$host\"\s*/>" src/chrome/content/rules/*.xml | wc -l`
+    REGEX_ESCAPED_HOST=`python3.6 -c "import re; print(re.escape('$host'))"`
+    REPEATS=`egrep -l "<target\s+host=\s*\"$REGEX_ESCAPED_HOST\"\s*/>" src/chrome/content/rules/*.xml | wc -l`
     if [ $REPEATS -gt 1 ]; then
         echo $host
     fi
