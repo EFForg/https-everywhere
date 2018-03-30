8cfaec6687c5222f7666073d7974556be7c6e8f3
William Budington <bill@eff.org>
2018-03-30 15:00:51 -0700

    Update periodicity to two days

--- chromium/background-scripts/update.js
+++ chromium/background-scripts/update.js
@@ -31,7 +31,7 @@ let store,
   background_callback;
 
 // how often we should check for new rulesets
-const periodicity = 10;
+const periodicity = 7200;
 
 // jwk key loaded from keys.js
 let imported_keys = {};
