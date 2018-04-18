2166ca3ca8d64af628d5b37b20cfa623d437ddf8
Gavin Lloyd <gavinhungry@gmail.com>
2018-04-18 14:28:06 -0700

    Add "Show Devtools tab" option to hide CDT tab (#15192)

--- chromium/pages/options/ux.js
+++ chromium/pages/options/ux.js
@@ -33,17 +33,20 @@ document.addEventListener("DOMContentLoaded", () => {
   const showCounter = document.getElementById("showCounter");
   const autoUpdateRulesets = document.getElementById("autoUpdateRulesets");
   const enableMixedRulesets = document.getElementById("enableMixedRulesets");
+  const showDevtoolsTab = document.getElementById("showDevtoolsTab");
 
   const defaultOptions = {
     showCounter: true,
     autoUpdateRulesets: true,
     enableMixedRulesets: false,
+    showDevtoolsTab: true
   };
 
   sendMessage("get_option", defaultOptions, item => {
     showCounter.checked = item.showCounter;
     autoUpdateRulesets.checked = item.autoUpdateRulesets;
     enableMixedRulesets.checked = item.enableMixedRulesets;
+    showDevtoolsTab.checked = item.showDevtoolsTab;
 
     showCounter.addEventListener("change", () => {
       sendMessage("set_option", { showCounter: showCounter.checked });
@@ -55,7 +58,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
     enableMixedRulesets.addEventListener("change", () => {
       sendMessage("set_option", { enableMixedRulesets: enableMixedRulesets.checked });
-    })
+    });
+
+    showDevtoolsTab.addEventListener("change", () => {
+      sendMessage("set_option", { showDevtoolsTab: showDevtoolsTab.checked });
+    });
   });
 
   document.onkeydown = function(evt) {
