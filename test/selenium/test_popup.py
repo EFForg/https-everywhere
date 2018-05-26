2d926550591f19d12bdcafeb2d8fbeb01d999473
Pasu Chan Chak Shing <ccspasu@gmail.com>
2018-05-26 15:20:26 +0800

    Fix selenium errors with correct CSS selectors

--- test/selenium/test_popup.py
+++ test/selenium/test_popup.py
@@ -5,7 +5,7 @@ class TestPopup(ExtensionTestCase):
     def test_rule_shown(self):
         url = 'http://freerangekitten.com'
         with self.load_popup_for(url):
-            el = self.query_selector('#StableRules > div > label > span')
+            el = self.query_selector('#StableRules > div > label')
             self.assertTrue(el.text.lower() in url)
 
     def test_disable_enable(self):
