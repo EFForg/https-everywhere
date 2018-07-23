198c77835a5bc6ece636e060ae83eeddcec12318
James Vasile <james@jamesvasile.com>
2018-07-23 13:55:13 -0400

    Use sudo to link chromedriver

--- install-dev-dependencies.sh
+++ install-dev-dependencies.sh
@@ -46,7 +46,7 @@ if type apt-get >/dev/null ; then
     $SUDO_SHIM chmod 755 /usr/bin/geckodriver
   fi
   if [ ! -f /usr/lib/chromium/chromedriver ] && [ -f `which chromedriver` ]; then
-    ln -s `which chromedriver` /usr/lib/chromium/chromedriver
+    $SUDO_SHIM ln -s `which chromedriver` /usr/lib/chromium/chromedriver
   fi
 elif type brew >/dev/null ; then
   brew list python &>/dev/null || brew install python
