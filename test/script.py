df4fa8fa5f6d046f804bb583d93defadfa245bfe
A Silent Cat <36388139+s1l3ntcat@users.noreply.github.com>
2018-08-01 16:48:48 -0500

    Removed seemingly redundant colors in script.py (#16062)

--- test/script.py
+++ test/script.py
@@ -16,14 +16,9 @@ from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
 class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
     OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
     FAIL = '\033[91m'
     ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
 
 
 if sys.argv[1] == "Chrome":
