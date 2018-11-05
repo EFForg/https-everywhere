73f31792272b79cbe4b8df8a703e94cd5cd7fd65
Alexis Hancock <lxshancock@gmail.com>
2018-11-05 13:21:32 -0800

    Revert "Remove https to https reference checks and schema"
    This reverts commit 04ac456075b0fcde5e7eae362407dbf27618303e.

--- test/validations/relaxng/run.py
+++ test/validations/relaxng/run.py
@@ -10,6 +10,7 @@ from lxml import etree
 
 parser = argparse.ArgumentParser(description="Validate rulesets against relaxng schema.xml")
 parser.add_argument("--source_dir", default="src/chrome/content/rules")
+parser.add_argument("--https2https_whitelist", default="utils/ruleset-whitelist.csv")
 
 args = parser.parse_args()
 
@@ -17,6 +18,18 @@ files = glob.glob(os.path.join(args.source_dir, "*.xml"))
 
 relaxng_doc = etree.parse("test/validations/relaxng/schema.xml")
 relaxng = etree.RelaxNG(relaxng_doc)
+
+relaxng_doc_https2https = etree.parse("test/validations/relaxng/schema_https2https.xml")
+relaxng_https2https = etree.RelaxNG(relaxng_doc_https2https)
+
+https2https_whitelist = {}
+
+with open(args.https2https_whitelist) as f:
+    f.readline()
+    for line in f:
+        fileHash, _, _, fileSkip, fileName = line.strip().split(",")
+        if fileSkip == "1":
+            https2https_whitelist[fileName] = fileHash
 
 exit_code = 0
 
@@ -28,6 +41,11 @@ for filename in sorted(files):
     basename = os.path.basename(filename)
 
     ruleset_relaxng = relaxng
+
+    if basename in https2https_whitelist:
+        with open(filename, "rb") as file:
+            if hashlib.sha256(file.read()).hexdigest() == https2https_whitelist[basename]:
+                ruleset_relaxng = relaxng_https2https
 
     if not ruleset_relaxng.validate(tree):
         exit_code = 1
