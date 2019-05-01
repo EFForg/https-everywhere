9b62b18a0dbcfb59735d8bf94cb1c07c8b022ec4
Alexis Hancock <lxshancock@gmail.com>
2019-05-01 14:24:01 -0700

    Amend method of grabbing elements for test
    - The plain visibility test of "seeing" an element by Selenium breaks on Chrome
    - The checkbox being visibily hidden by CSS will break on the Chrome unit tests
    - Changed method to making sure checkbox ID is present and then checking if selected

--- test/selenium/test_popup.py
+++ test/selenium/test_popup.py
@@ -12,47 +12,49 @@ class TestPopup(ExtensionTestCase):
 
     def test_disable_enable(self):
         selector = '#onoffswitch__label'
-        checkbox = '#onoffswitch'
+        checkbox = 'onoffswitch'
         var_name = 'background.state.isExtensionEnabled'
 
         # disable https everywhere
         with self.load_popup_for():
             el = self.query_selector(selector)
-            checkbox_el = self.query_selector(checkbox)
+            # Using query_selector does not work here if element isn't "plainly" visible on page, fails on Chrome
+            checkbox_el = self.driver.find_element_by_id(checkbox)
             self.assertTrue(checkbox_el.is_selected())
             el.click() # disable
 
         with self.load_popup_for():
             el = self.query_selector(selector)
-            checkbox_el = self.query_selector(checkbox)
+            checkbox_el = self.driver.find_element_by_id(checkbox)
             self.assertFalse(checkbox_el.is_selected())
             el.click()
 
         with self.load_popup_for():
             el = self.query_selector(selector)
-            checkbox_el = self.query_selector(checkbox)
+            checkbox_el = self.driver.find_element_by_id(checkbox)
             self.assertTrue(checkbox_el.is_selected())
 
     def test_http_nowhere(self):
         selector = '#http-nowhere-checkbox_label'
-        checkbox = '#http-nowhere-checkbox'
+        checkbox = 'http-nowhere-checkbox'
         var_name = 'background.state.httpNowhereOn'
 
         # check default state and enable
         with self.load_popup_for():
             el = self.query_selector(selector)
-            checkbox_el = self.query_selector(checkbox)
+            # Using query_selector does not work here if element isn't "plainly" visible on page, fails on Chrome
+            checkbox_el = self.driver.find_element_by_id(checkbox)
             self.assertFalse(checkbox_el.is_selected())
             el.click()
 
         # check it is enabled, and disable
         with self.load_popup_for():
             el = self.query_selector(selector)
-            checkbox_el = self.query_selector(checkbox)
+            checkbox_el = self.driver.find_element_by_id(checkbox)
             self.assertTrue(checkbox_el.is_selected())
             el.click() # disable
 
         # check disabled
         with self.load_popup_for():
-            checkbox_el = self.query_selector(checkbox)
+            checkbox_el = self.driver.find_element_by_id(checkbox)
             self.assertFalse(checkbox_el.is_selected()) # default state
