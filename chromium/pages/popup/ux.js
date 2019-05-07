e789df54b348dfbf77e855358b3dcb06f5d55bba
Chan Chak Shing <ccspasu@gmail.com>
2019-05-07 20:31:29 +0800

    Update chromium/pages/popup/ux.js, fix #17916

--- chromium/pages/popup/ux.js
+++ chromium/pages/popup/ux.js
@@ -13,8 +13,8 @@
 function toggleRuleLine(event) {
   getTab(activeTab => {
     const set_ruleset = {
-      active: event.target.parentNode.firstChild.checked,
-      name: event.target.innerText,
+      active: event.target.checked,
+      name: event.target.parentNode.innerText,
       tab_id: activeTab.id,
     };
 
@@ -111,6 +111,9 @@ function appendRulesToListDiv(rulesets, list_div, ruleType) {
       checkbox.checked = ruleset.active;
       text.innerText = ruleset.name;
 
+      // Add listener to capture the toggle event
+      line.addEventListener("click", toggleRuleLine);
+
       if (ruleset.note && ruleset.note.length) {
         line.title = ruleset.note;
 
@@ -187,9 +190,6 @@ function listRules(activeTab) {
 
       appendRulesToListDiv(stableRules, e("StableRules"), 'stable');
       appendRulesToListDiv(unstableRules, e("UnstableRules"), 'unstable');
-
-      // Add listener to capture the toggle event
-      e("add-new-rule-button").addEventListener("click", toggleRuleLine);
     }
 
     // Only show the "Add a rule" section if we're on an HTTPS page
