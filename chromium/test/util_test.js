b3c52def449047d17e73091fcad77247c75faf09
Chan Chak Shing <ccspasu@gmail.com>
2019-10-26 22:10:20 +0800

    Update test/util_test.js, fix typo

--- chromium/test/util_test.js
+++ chromium/test/util_test.js
@@ -1,44 +1,44 @@
 'use strict';
 
 const assert = require('chai').assert,
-  utils = require('../background-scripts/util');
+  util = require('../background-scripts/util');
 
 
-describe('utils.js', function() {
+describe('util.js', function() {
   describe('nullIterable', function() {
     it('is iterable zero times and is size 0', function() {
       let count = 0;
-      for (let _ of utils.nullIterable) { // eslint-disable-line no-unused-vars
+      for (let _ of util.nullIterable) { // eslint-disable-line no-unused-vars
         count += 1;
       }
       assert.strictEqual(count, 0);
-      assert.strictEqual(utils.nullIterable.size,  0);
-      assert.isEmpty(utils.nullIterable);
+      assert.strictEqual(util.nullIterable.size,  0);
+      assert.isEmpty(util.nullIterable);
     });
   });
 
   describe('getNormalisedHostname', function() {
     it('removes tailing dots', function() {
-      assert.strictEqual(utils.getNormalisedHostname('example.com.'), 'example.com');
-      assert.strictEqual(utils.getNormalisedHostname('example.com..'), 'example.com');
+      assert.strictEqual(util.getNormalisedHostname('example.com.'), 'example.com');
+      assert.strictEqual(util.getNormalisedHostname('example.com..'), 'example.com');
     });
 
     it('preserves a single dot', function() {
-      assert.strictEqual(utils.getNormalisedHostname('.'), '.');
+      assert.strictEqual(util.getNormalisedHostname('.'), '.');
     });
   });
 
   describe('getWildcardExpressions', function() {
     it('return empty result for ill-formed hosts', function() {
-      assert.strictEqual(utils.getWildcardExpressions('').size, 0);
-      assert.strictEqual(utils.getWildcardExpressions('example.com..').size, 0);
+      assert.strictEqual(util.getWildcardExpressions('').size, 0);
+      assert.strictEqual(util.getWildcardExpressions('example.com..').size, 0);
     });
 
     it('return empty result for wildcard hosts', function() {
-      assert.strictEqual(utils.getWildcardExpressions('example.*').size, 0);
-      assert.strictEqual(utils.getWildcardExpressions('example.com.*').size, 0);
-      assert.strictEqual(utils.getWildcardExpressions('*.example.com').size, 0);
-      assert.strictEqual(utils.getWildcardExpressions('*.subdomain.example.com').size, 0);
+      assert.strictEqual(util.getWildcardExpressions('example.*').size, 0);
+      assert.strictEqual(util.getWildcardExpressions('example.com.*').size, 0);
+      assert.strictEqual(util.getWildcardExpressions('*.example.com').size, 0);
+      assert.strictEqual(util.getWildcardExpressions('*.subdomain.example.com').size, 0);
     });
 
     it('return list of supported wildcard expression', function() {
@@ -59,7 +59,7 @@ describe('utils.js', function() {
       };
 
       for (const host in params) {
-        assert.deepEqual(utils.getWildcardExpressions(host), params[host]);
+        assert.deepEqual(util.getWildcardExpressions(host), params[host]);
       }
     });
   });
