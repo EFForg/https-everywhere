da793bb7ea111a0d1d38c42b8372e54e934f8194
William Budington <bill@eff.org>
2019-11-07 11:32:00 -0800

    Ensure that the translations script does not attempt to convert nonexistant translation files (#18667)

--- utils/chromium-translations.py
+++ utils/chromium-translations.py
@@ -15,23 +15,25 @@ dest_dir = sys.argv[2]
 message_regex = re.compile("<!ENTITY https-everywhere\.([\w.-]+) \"(.*?)\">")
 
 def convert(locale):
-  target_messages = {}
-  with open(os.path.join(source_dir, locale, "https-everywhere.dtd"), 'r', encoding='utf-8') as f:
-    for line in f:
-      m = message_regex.search(line)
-      if m:
-        message_name = m.group(1)
-        message_value = m.group(2)
-        message_name = re.sub("[.-]", "_", message_name)
-        target_messages[message_name] = {
-          "message": message_value
-        }
-  target_dir = os.path.join(dest_dir, locale)
-  if not os.path.isdir(target_dir):
-    os.mkdir(target_dir)
-  with open(os.path.join(target_dir, "messages.json"), "w") as out_file:
-    out_file.write(json.dumps(target_messages, sort_keys=True, indent=4))
+  translation_file = os.path.join(source_dir, locale, "https-everywhere.dtd")
+  if os.path.isfile(translation_file):
+    target_messages = {}
+    with open(translation_file, 'r', encoding='utf-8') as f:
+      for line in f:
+        m = message_regex.search(line)
+        if m:
+          message_name = m.group(1)
+          message_value = m.group(2)
+          message_name = re.sub("[.-]", "_", message_name)
+          target_messages[message_name] = {
+            "message": message_value
+          }
+    target_dir = os.path.join(dest_dir, locale)
+    if not os.path.isdir(target_dir):
+      os.mkdir(target_dir)
+    with open(os.path.join(target_dir, "messages.json"), "w") as out_file:
+      out_file.write(json.dumps(target_messages, sort_keys=True, indent=4))
 
 for locale in os.listdir(source_dir):
   if not "." in locale:
-      convert(locale)
+    convert(locale)
