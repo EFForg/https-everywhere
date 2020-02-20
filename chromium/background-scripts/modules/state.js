89576e6ac2f2affce1c3d1f9925fa2c6362f9924
Alexis Hancock <alexis@eff.org>
2020-02-19 17:20:28 -0800

    remove console logs

--- chromium/background-scripts/modules/state.js
+++ chromium/background-scripts/modules/state.js
@@ -14,20 +14,17 @@ let validation = require('./validation');
  */
 function isExtensionDisabledOnSite(host, httpOnceList, disabledList) {
   // make sure the host is not matched in the httpOnceList
-  console.log(httpOnceList);
   if (httpOnceList.has(host)) {
     return true;
   }
 
   // make sure the host is not matched in the disabledList
-  console.log(disabledList);
   if (disabledList.has(host)) {
     return true;
   }
 
   // make sure the host is matched by any wildcard expressions in the disabledList
   const expressions = validation.getWildcardExpressions(host);
-  console.log(expressions);
   for (const expression of expressions) {
     if (disabledList.has(expression)) {
       return true;
