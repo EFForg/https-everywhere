b8ca843fa8ec18292368c6d06d5f92f46574e6d8
Chan Chak Shing <ccspasu@gmail.com>
2020-04-25 09:52:26 +0800

    Update test/selenium/test_navigation.py

--- test/selenium/test_navigation.py
+++ test/selenium/test_navigation.py
@@ -21,7 +21,14 @@ class TestNavigation(ExtensionTestCase):
     def test_httpnowhere_blocks(self):
         #if self.shim.browser_type == 'firefox':
         #    raise unittest.SkipTest('broken on firefox')
-        href_script = 'return window.location.href;'
+        href_script = """(async () => {
+          return await new Promise(resolve => {
+            setTimeout(() => {
+              resolve(window.href.location);
+            }, 1000);
+          })
+        })();
+        """
         self.toggle_http_nowhere()
         self.driver.get(http_url)
         expected_href_location = self.driver.execute_script(href_script)
