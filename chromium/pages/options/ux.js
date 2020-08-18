71e82d6761396631ab3c8830b01b408f5aa9dcf9
William Budington <bill@eff.org>
2020-08-18 10:20:03 -0700

    Update chromium/pages/options/ux.js
    Co-authored-by: pipboy96 <pipboy96@protonmail.com>

--- chromium/pages/options/ux.js
+++ chromium/pages/options/ux.js
@@ -331,14 +331,14 @@ document.addEventListener("DOMContentLoaded", () => {
   const disabled_site_input = document.getElementById("disabled-site");
   const add_disabled_site_invalid_host = document.getElementById('add-disabled-site-invalid-host');
   disabled_site_input.setAttribute("placeholder", chrome.i18n.getMessage("options_enterDisabledSite"));
-function isValidHost(host) {
-  try {
-    new URL(`http://${host}/`);
-    return true;
-  } catch {
-    return false;
+  function isValidHost(host) {
+    try {
+      new URL(`http://${host}/`);
+      return true;
+    } catch {
+      return false;
+    }
   }
-}
   add_disabled_site.addEventListener("click", function() {
     const host = disabled_site_input.value;
 
