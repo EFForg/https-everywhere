98bdbb10091668301cea6fddf903d613dd795b4d
Robin Schneider <ypid@riseup.net>
2020-12-19 18:50:19 +0100

    Move rewriter docs from script comment to previously empty README

---
+++ utils/rewriter/README.md
@@ -0,0 +1,23 @@
+# HTTPS Rewriter
+
+Uses the rulesets from HTTPS to recursively rewrite URL references in a
+given directory to HTTPS. Uses protocol-relative URLs wherever possible.
+Makes a copy of each file at filename.bak.
+
+## Install
+
+```Shell
+cd https-everywhere
+./install-dev-dependencies.sh
+./make.sh # to build default.rulesets
+cd utils/rewriter/
+npm install
+```
+
+## Usage
+
+```Shell
+node rewriter.js ~/path/to/my/webapp
+cd ~/path/to/my/webapp
+git diff
+```
--- utils/rewriter/rewriter.js
+++ utils/rewriter/rewriter.js
@@ -1,19 +1,3 @@
-// HTTPS Rewriter.
-//
-// Uses the rulesets from HTTPS to recursively rewrite URL references in a
-// given directory to HTTPS. Uses protocol-relative URLs wherever possible.
-// Makes a copy of each file at filename.bak.
-//
-// Usage:
-//  cd https-everywhere
-//  ./install-dev-dependencies.sh
-//  ./make.sh # to build default.rulesets
-//  cd utils/rewriter/
-//  npm install
-//  node rewriter.js ~/path/to/my/webapp
-//  cd ~/path/to/my/webapp
-//  git diff
-
 global.self = global;
 TextDecoder = require('text-encoding').TextDecoder;
 TextEncoder = require('text-encoding').TextEncoder;
