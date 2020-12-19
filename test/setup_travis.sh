1338ec376938a40fb7bb9b468d469f69798ee069
cschanaj <cschanaj@users.noreply.github.com>
2020-12-19 05:05:13 +0100

    Install the latest geckodriver for firefox

--- test/setup_travis.sh
+++ test/setup_travis.sh
@@ -17,11 +17,14 @@ function setup_chrome {
 }
 
 function setup_firefox {
-    #version=$(curl -s https://api.github.com/repos/mozilla/geckodriver/releases/latest | grep tag_name | cut -d '"' -f 4)
-    # hard-code latest version, since apparently the above `curl` is fucking up in travis
-    version="v0.26.0"
-    url="https://github.com/mozilla/geckodriver/releases/download/${version}/geckodriver-${version}-linux64.tar.gz"
-    wget -O /tmp/geckodriver.tar.gz ${url}
+    # install the latest version of geckodriver (per Github release API)
+    firefox_version=$("${BROWSER}" -version)
+    geckodriver_version=$(curl -s https://api.github.com/repos/mozilla/geckodriver/releases/latest | grep tag_name | cut -d '"' -f 4)
+    geckodriver_url="https://github.com/mozilla/geckodriver/releases/download/${version}/geckodriver-${version}-linux64.tar.gz"
+
+    echo "Setting up geckodriver ${geckodriver_version} for ${1} ${firefox_version}"
+
+    wget -O /tmp/geckodriver.tar.gz ${geckodriver_url}
     sudo tar -xvf /tmp/geckodriver.tar.gz -C /usr/local/bin/
     sudo chmod a+x /usr/local/bin/geckodriver
 }
