d0b4b0f839f0221854a534d7ea1b84c9c57b77dc
Chan Chak Shing <cschanaj@users.noreply.github.com>
2021-06-10 01:26:38 +0200

    Fix unexpected arithmetic operations on strings (#20043)
    * Update ip_utils.js
    
    * Update ip_utils.js
    
    * Update ip_utils.js

--- chromium/background-scripts/ip_utils.js
+++ chromium/background-scripts/ip_utils.js
@@ -2,11 +2,17 @@
 
 (function (exports) {
 
+/**
+ * Parse and convert literal IP address into numerical IP address.
+ * @param {string} ip
+ * @returns {number}
+ */
 const parseIp = ip => {
   if (!/^[0-9.]+$/.test(ip)) {
     return -1;
   }
 
+  /** @type {string[]} */
   const octets = ip.split('.');
 
   if (octets.length !== 4) {
@@ -26,14 +32,21 @@ const parseIp = ip => {
       return -1;
     }
 
-    ipN = (ipN << 8) | octet;
+    ipN = (ipN << 8) | octetN;
   }
 
   return ipN >>> 0;
 };
 
+/**
+ * Check if the numeric IP address is within a certain range.
+ * @param {number} ip
+ * @param {number[]} range
+ * @returns {boolean}
+ */
 const isIpInRange = (ip, [rangeIp, mask]) => (ip & mask) >>> 0 === rangeIp;
 
+// A list of local IP address ranges
 const localRanges = [
   [/* 0.0.0.0         */ 0x00000000, /* 255.255.255.255 */ 0xffffffff],
   [/* 127.0.0.0       */ 0x7f000000, /* 255.0.0.0       */ 0xff000000],
@@ -42,6 +55,11 @@ const localRanges = [
   [/* 192.168.0.0     */ 0xc0a80000, /* 255.255.0.0     */ 0xffff0000],
 ];
 
+/**
+ * Check if the numeric IP address is inside the local IP address ranges.
+ * @param {number} ip
+ * @returns {boolean}
+ */
 const isLocalIp = ip => localRanges.some(range => isIpInRange(ip, range));
 
 Object.assign(exports, {
